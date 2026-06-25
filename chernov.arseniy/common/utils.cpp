b636620ecc8801e4d888ade2f3902444d63a6b68
aaaa24 <chernov.arseniy@yandex.ru>
2026-06-25 13:01:14 +0300

    add rebuildAllIds

--- chernov.arseniy/common/utils.cpp
+++ chernov.arseniy/common/utils.cpp
@@ -19,3 +19,31 @@ bool chernov::hasId(const Vector< size_t > & allIds, size_t id)
   }
   return false;
 }
+
+void chernov::rebuildAllIds(Vector< size_t > & allIds,
+  const Vector< Person > & persons,
+  const Vector< Meeting > & meetings)
+{
+  Vector< size_t > newAllIds;
+  init(newAllIds);
+
+  try {
+    for (size_t i = 0; i < persons.size; ++i) {
+      addUniqueId(newAllIds, persons.data[i].id);
+    }
+    for (size_t i = 0; i < meetings.size; ++i) {
+      addUniqueId(newAllIds, meetings.data[i].from);
+      addUniqueId(newAllIds, meetings.data[i].to);
+    }
+  } catch (...) {
+    destroy(newAllIds);
+    throw;
+  }
+
+  destroy(allIds);
+  allIds.data = newAllIds.data;
+  allIds.size = newAllIds.size;
+  allIds.capacity = newAllIds.capacity;
+
+  init(newAllIds);
+}
--- chernov.arseniy/common/utils.hpp
+++ chernov.arseniy/common/utils.hpp
@@ -1,12 +1,15 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include "meeting.hpp"
+#include "person.hpp"
 #include "vector.hpp"
 
 namespace chernov {
 
   void addUniqueId(Vector< size_t > & allIds, size_t id);
   bool hasId(const Vector< size_t > & allIds, size_t id);
+  void rebuildAllIds(Vector< size_t > & allIds, const Vector< Person > & persons, const Vector< Meeting > & meetings);
 
 }
 
