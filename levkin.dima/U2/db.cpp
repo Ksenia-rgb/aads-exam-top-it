5a771f131ab0973ff637ed15dea735a2dfab1f38
Dmitry Levkin <mit.levkin@vk.com>
2026-06-25 13:12:42 +0300

    Add helper functions for DB person management

--- levkin.dima/U2/db.cpp
+++ levkin.dima/U2/db.cpp
@@ -7,5 +7,30 @@ namespace levkin {
     freeVec(db.persons);
     freeVec(db.meetings);
   }
+  bool hasPerson(const DB& db, size_t id)
+  {
+    for (size_t i = 0; i < db.persons.size; ++i) {
+      if (db.persons.data[i].first == id)
+        return true;
+    }
+    return false;
+  }
 
+  bool hasDescription(const DB& db, size_t id)
+  {
+    for (size_t i = 0; i < db.persons.size; ++i) {
+      if (db.persons.data[i].first == id) {
+        return !db.persons.data[i].second.empty();
+      }
+    }
+    return false;
+  }
+
+  void ensurePersonExists(DB& db, size_t id)
+  {
+    if (!hasPerson(db, id)) {
+      Pair p = std::make_pair(id, "");
+      pushBack(db.persons, p);
+    }
+  }
 }
