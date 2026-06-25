eb6baf40bf97dd5f71f4182596b84ed60669d63b
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-06-25 13:54:58 +0300

    Correct interface

--- muhamadiarov.insaf/U1/main.cpp
+++ muhamadiarov.insaf/U1/main.cpp
@@ -33,6 +33,31 @@ namespace muhamadiarov
     }
     return in;
   }
+
+  bool check(size_t* numbers, size_t& size, size_t& count, size_t id)
+  {
+    for (size_t i = 0; i < size; ++i)
+    {
+      if (numbers[i] == id)
+      {
+        return false;
+      }
+    }
+    size_t* newNumbers = nullptr;
+    if (size == count)
+    {
+      newNumbers = new size_t[count * 2];
+      count = count * 2;
+      for (size_t i = 0; i < size; ++i)
+      {
+        newNumbers[i] = numbers[i];
+      }
+      newNumbers[size] = id;
+      delete [] numbers;
+      numbers = newNumbers;
+    }
+    return true;
+  }
 }
 
 int main(int argc, char *argv[])
@@ -42,6 +67,9 @@ int main(int argc, char *argv[])
   size_t id;
   std::string data;
   muh::Queue< muh::Person > queue;
+  size_t count = 10;
+  size_t size = 0;
+  size_t* numbers = new size_t[count];
   if (argc == 2)
   {
     std::ifstream file(argv[1]);
@@ -55,12 +83,16 @@ int main(int argc, char *argv[])
     {
       while (file >> id && muh::getLine(file, data))
       {
-        queue.push({id, data});
+        if (!data.empty() && muh::check(numbers, size, count, id))
+        {
+          queue.push({id, data});
+        }
       }
     }
     catch (const std::exception& e)
     {
       std::cerr << "Incorrect data: " << e.what() << '\n';
+      delete [] numbers;
       return 1;
     }
     file.close();
@@ -69,16 +101,20 @@ int main(int argc, char *argv[])
   {
     while (std::cin >> id && muh::getLine(std::cin, data))
     {
-      queue.push({id, data});
+      if (!data.empty() && muh::check(numbers, size, count, id))
+      {
+        queue.push({id, data});
+      }
     }
   }
   else
   {
     std::cerr << "Incorrect usage\n";
+    delete [] numbers;
     return 1;
   }
-
-  size_t size = queue.size();
+  delete [] numbers;
+  size = queue.size();
   for (size_t i = 0; i < size; ++i)
   {
     muh::Person& person = queue.top();
