ebbb58fa29ad71f53db2c57ae1b5d166871c2645
liia <kirilich_lubit@rambler.ru>
2026-06-25 14:02:18 +0300

    add reverse and pushback for dynamicarray

--- nabieva.liia/U2/dynamic-array.hpp
+++ nabieva.liia/U2/dynamic-array.hpp
@@ -30,6 +30,45 @@ namespace nabieva
     array.size = 0;
     array.capacity = 0;
   }
+
+  template< typename T >
+  void reserve(DynamicArray< T >& array, std::size_t newCapacity)
+  {
+    if (newCapacity <= array.capacity)
+    {
+      return;
+    }
+
+    T* newData = new T[newCapacity];
+    try
+    {
+      for (std::size_t i = 0; i < array.size; ++i)
+      {
+        newData[i] = array.data[i];
+      }
+    }
+    catch (...)
+    {
+      delete[] newData;
+      throw;
+    }
+
+    delete[] array.data;
+    array.data = newData;
+    array.capacity = newCapacity;
+  }
+
+  template< typename T >
+  void pushBack(DynamicArray< T >& array, const T& value)
+  {
+    if (array.size == array.capacity)
+    {
+      const std::size_t newCapacity = (array.capacity == 0 ? 4 : array.capacity * 2);
+      reserve(array, newCapacity);
+    }
+    array.data[array.size] = value;
+    ++array.size;
+  }
 }
 
 #endif
