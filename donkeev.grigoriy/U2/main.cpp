ee80e6f3b7164eda992e13a92dde013fb6acb7ea
grisha-git <donkeevgrisa@gmail.com>
2026-06-25 13:19:24 +0300

    Reading par

--- donkeev.grigoriy/U2/main.cpp
+++ donkeev.grigoriy/U2/main.cpp
@@ -1,6 +1,30 @@
+#include <fstream>
+#include <iostream>
+
 #include "list.hpp"
 
-int main()
+
+int main(int argc, char* argv[])
 {
+  std::istream* input = &std::cin;
+
+  std::ifstream inFile;
+
+  donkeev::PersonList persons;
+  persons.head_ = nullptr;
+  persons.tail_ = nullptr;
+  persons.size_ = 0;
   
-}
+  if (argc > 3)
+  {
+    std::cerr << "Bad parametrs\n";
+    return 1;
+  }
+  else if (argc == 3)
+  {
+
+  }
+
+  int inCount = 0;
+  int outCount = 0;
+
