4a7818baee0138c8350952fba3fe8aa239763e38
zub <ars1for1work@gmail.com>
2026-06-25 11:45:41 +0300

    feat(main.cpp): add command-line argument parsing and file handling for input/output redirection

--- zubarev.arsenii/U1/main.cpp
+++ zubarev.arsenii/U1/main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 #include "functions.hpp"
 #include "person.hpp"
-int main()
+
+int main(int argc, char* argv[])
 {
-  
+  std::string in_name = "";
+  std::string out_name = "";
+
+  if (!zubarev::detail::parse_args(argc, argv, in_name, out_name)) {
+    return 1;
+  }
+
+  std::istream* input = &std::cin;
+  std::ifstream in_file;
+
+  if (!in_name.empty()) {
+    in_file.open(in_name);
+    if (!in_file) {
+      return 2;
+    }
+    input = std::addressof(in_file);
+  }
+
+  std::ostream* output = &std::cout;
+  std::ofstream out_file;
+
+  if (!out_name.empty()) {
+    out_file.open(out_name);
+    if (!out_file) {
+      return 2;
+    }
+    output = std::addressof(out_file);
+  }
+
+  zubarev::PersonArray persons;
+  zubarev::HashSet ids;
+
+  init(persons);
+  init(ids);
+
+  destroy(persons);
+  destroy(ids);
 }
