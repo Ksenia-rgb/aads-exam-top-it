a263b95aacfc00e2ddd18aed8947251351d70e5d
EGGorkaa <egor_novikov20004@mail.ru>
2026-06-25 12:57:20 +0300

    fixes for actions

---
+++ novikov.egor/U1/List.hpp
@@ -0,0 +1,47 @@
+#ifndef LIST_HPP
+#define LIST_HPP
+
+namespace novikov
+{
+  template < class T >
+  struct Node
+  {
+    T val;
+    Node< T > *next;
+  };
+
+  template < class T >
+  struct List
+  {
+    Node< T > *head;
+  };
+
+  template < class T >
+  void push_back(List< T > &list, const T &val)
+  {
+    Node< T > *node = new Node< T >{val, nullptr};
+    if (!list.head) {
+      list.head = node;
+    } else {
+      Node< T > *cur = list.head;
+      while (cur->next) {
+        cur = cur->next;
+      }
+      cur->next = node;
+    }
+  };
+
+  template < class T >
+  void clear(List<T>& list)
+  {
+    Node<T>* cur = list.head;
+    while(cur)
+    {
+      Node<T>* next = cur->next;
+      delete cur;
+      cur = next;
+    }
+    list.head = nullptr;
+  };
+}
+#endif
---
+++ novikov.egor/U1/Person.hpp
@@ -0,0 +1,16 @@
+#ifndef PERSON_HPP
+#define PERSON_HPP
+
+#include <string>
+
+namespace novikov
+{
+  struct Person
+  {
+    size_t id;
+    std::string info;
+  };
+
+}
+
+#endif
