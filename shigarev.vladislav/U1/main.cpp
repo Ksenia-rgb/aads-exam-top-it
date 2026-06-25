c8b31902873e9642703c27d8551d1c46b456b82a
Владислав  Шигарев <vladislavsigarev@MBPWiFi.local>
2026-06-25 11:48:16 +0300

    Add main file

---
+++ shigarev.vladislav/U1/main.cpp
@@ -0,0 +1,45 @@
+#include "parseargs.hpp"
+#include "personio.hpp"
+
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  shigarev::Args args = {"", "", false, false};
+  if (!shigarev::parseArgs(argc, argv, args)) {
+    std::cerr << "Invalid arguments\n";
+    return 1;
+  }
+
+  std::ifstream inputFile;
+  if (args.hasInputFile) {
+    inputFile.open(args.inputFile);
+    if (!inputFile.is_open()) {
+      std::cerr << "Cannot open input file\n";
+      return 2;
+    }
+  }
+  std::istream& in = args.hasInputFile
+      ? inputFile
+      : std::cin;
+
+  std::ofstream outputFile;
+  if (args.hasOutputFile) {
+    outputFile.open(args.outputFile);
+    if (!outputFile.is_open()) {
+      std::cerr << "Cannot open output file\n";
+      return 2;
+    }
+  }
+  std::ostream& out = args.hasOutputFile
+      ? outputFile
+      : std::cout;
+
+  shigarev::ReadResult result = shigarev::readPersons(in);
+  shigarev::writePersons(out, result.persons);
+  shigarev::writeStats(std::cerr, result.readCount,
+      result.ignoredCount);
+  shigarev::destroyDynArray(result.persons);
+  return 0;
+}
