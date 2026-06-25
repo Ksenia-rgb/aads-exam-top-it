827829f6cba63b665cf2569b08b1c2c1ae1615eb
DalveShutnik <119686844+DalveShutnik@users.noreply.github.com>
2026-06-25 11:24:31 +0300

    name process exit codes

--- samarin.denis/U1/main.cpp
+++ samarin.denis/U1/main.cpp
@@ -8,6 +8,9 @@
 #include "records.hpp"
 
 namespace {
+  const int exitInvalidArguments = 1;
+  const int exitRuntimeError = 2;
+
   int writeResult(const samarin::options_t & options,
       const samarin::detail::list_t< samarin::Person > & records,
       const samarin::counts_t & counts)
@@ -18,14 +21,14 @@ namespace {
     }
     if (options.hasOutput && !outputFile.is_open()) {
       std::cerr << "cannot open output file\n";
-      return 2;
+      return exitRuntimeError;
     }
     std::ostream & output = options.hasOutput ? outputFile : std::cout;
     samarin::writeRecords(output, records);
     std::cerr << counts.accepted << ' ' << counts.ignored << '\n';
     if (!output) {
       std::cerr << "cannot write output\n";
-      return 2;
+      return exitRuntimeError;
     }
     return 0;
   }
@@ -36,7 +39,7 @@ int main(int argc, char ** argv)
   samarin::options_t options{ false, "", false, "" };
   if (!samarin::parseArguments(argc, argv, options)) {
     std::cerr << "invalid arguments\n";
-    return 1;
+    return exitInvalidArguments;
   }
 
   std::ifstream inputFile;
@@ -44,7 +47,7 @@ int main(int argc, char ** argv)
     inputFile.open(options.inputName);
     if (!inputFile.is_open()) {
       std::cerr << "cannot open input file\n";
-      return 2;
+      return exitRuntimeError;
     }
   }
   std::istream & input = options.hasInput ? inputFile : std::cin;
@@ -59,7 +62,7 @@ int main(int argc, char ** argv)
     code = writeResult(options, records, counts);
   } catch (const std::exception & error) {
     std::cerr << error.what() << '\n';
-    code = 2;
+    code = exitRuntimeError;
   }
   samarin::detail::clear(records);
   return code;
