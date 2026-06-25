af18f322fba37bb9e599b25d9b1fd9e5612cb9be
BukreevYas <bukreev.yas@edu.spbstu.ru>
2026-06-25 11:21:17 -0400

    Add basic list logic

---
+++ bukreev.yakov/U1/list.hpp
@@ -0,0 +1,41 @@
+#ifndef LIST_HPP
+#define LIST_HPP
+
+template< class T >
+struct List
+{
+  T val;
+  List< T >* next;
+  List< T >* prev;
+};
+
+template< class T >
+void initList(List< T >* fake) noexcept
+{
+  fake->next = fake;
+  fake->prev = fake;
+}
+
+template< class T >
+void insertListHead(List< T >* fake, const T& val)
+{
+  List< T >* newNode = new List< T >;
+  newNode.val = val;
+
+  newNode->prev = fake;
+  newNode->next = fake->next;
+  fake->next = newNode;
+}
+
+template< class T >
+void insertListTail(List< T >* fake, const T& val)
+{
+  List< T >* newNode = new List< T >;
+  newNode.val = val;
+
+  newNode->next = fake;
+  newNode->prev = fake->prev;
+  fake->prev = newNode;
+}
+
+#endif
