ee4536498fdb06057103deef559dfe88032210f8
jar <Jarafan3@gmail.com>
2026-06-25 11:13:37 +0300

    Add parseLine

--- afanasev.yaroslav/U1/main.cpp
+++ afanasev.yaroslav/U1/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 struct Person
 {
   size_t id;
   std::string info;
 };
+
+bool parseLine(const std::string & line, Person & person)
+{
+  std::istringstream iss(line);
+  if (!(iss >> person.id))
+  {
+    return false;
+  }
+
+  iss >> std::ws;
+  if (!std::getline(iss, person.info))
+  {
+    return false;
+  }
+  return !person.info.empty();
+}
 
 int main()
 {
@@ -15,15 +33,15 @@ int main()
   persons = nullptr;
   size_t capacity = 0;
   std::string line;
-  while (std::getline(in, line))
+
+  while (std::getline(std::cin, line))
   {
     Person person;
     if (parseLine(line, person))
     {
-            addPerson(persons, count, capacity, person);
+      addPerson(persons, count, capacity, person);
     }
   }
-
 
 
   return 0;
