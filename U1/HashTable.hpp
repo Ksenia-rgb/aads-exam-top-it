ef6963914ed7165b37c257277f8d55b0d0ebe6a1
EGGorkaa <egor_novikov20004@mail.ru>
2026-06-25 11:33:01 +0300

    add insert for Hash

--- U1/HashTable.hpp
+++ U1/HashTable.hpp
@@ -24,6 +24,23 @@ namespace novikov
     Hash hasher;
     Equal equal;
   };
+
+  template < class T, class Key, class Hash, class Equal >
+  void insert(HashTable< T, Key, Hash, Equal > &table, const Key &key, const T &val)
+  {
+    size_t index = table.hasher(key) % table.capacity;
+    HashNode< T, Key > *cur = table.data[index];
+    while (cur) {
+      if (table.equal(cur->key, key)) {
+        cur->val = val;
+        return;
+      }
+      cur = cur->next;
+    }
+    HashNode< T, Key > *node = new HashNode< T, Key >{val, key, table.data[index]};
+    table.data[index] = node;
+    table.size++;
+  };
 }
 
 #endif
