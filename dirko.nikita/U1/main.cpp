e001b7ab06b846690d503ec7d7863fa6d70a6020
Alastor20 <nik_d.007@mail.ru>
2026-06-25 11:17:11 +0300

    args hadling

--- dirko.nikita/U1/main.cpp
+++ dirko.nikita/U1/main.cpp
@@ -1,5 +1,7 @@
 #include <cstddef>
 #include <string>
+#include <fstream>
+#include <iostream>
 
 struct Person
 {
@@ -9,4 +11,47 @@ struct Person
 
 int main(int argc, char *argv[])
 {
+  if (argc > 3) {
+    return 1;
+  }
+
+  std::string inFilename = "";
+  std::string outFilename = "";
+  bool inSet = false;
+  bool outSet = false;
+
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i];
+
+    if (arg.find("in:") == 0) {
+      if (inSet)
+        return 1;
+      inFilename = arg.substr(3);
+      if (inFilename.empty())
+        return 1;
+      inSet = true;
+    } else if (arg.find("out:") == 0) {
+      if (outSet)
+        return 1;
+      outFilename = arg.substr(4);
+      if (outFilename.empty())
+        return 1;
+      outSet = true;
+    } else {
+      return 1;
+    }
+  }
+  if (inSet) {
+    std::ifstream fin(inFilename);
+    if (!fin.is_open()) {
+      return 2;
+    }
+  }
+
+  if (outSet) {
+    std::ofstream fout(outFilename);
+    if (!fout.is_open()) {
+      return 2;
+    }
+  }
 }
