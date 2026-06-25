7a06fa4f71379e00b531dc6f82a9a682d1aae7de
Yugo <95368001+Degr3e@users.noreply.github.com>
2026-06-25 11:57:40 +0300

    add duplicate id check

--- yalovsky.gleb/U1/Person.cpp
+++ yalovsky.gleb/U1/Person.cpp
@@ -45,5 +45,17 @@ namespace yalovsky
       info = line.substr(pos);
       return true;
     }
+
+    bool containsId(const Array< Person >& persons, size_t id)
+    {
+      for (size_t i = 0; i < persons.size; ++i)
+      {
+        if (persons.data[i].id == id)
+        {
+          return true;
+        }
+      }
+      return false;
+    }
   }
 }
