21186f5b5f991795c7aa5ab1bdebcf2e94c76056
s1nhronn <silekatik@yandex.ru>
2026-06-25 11:49:26 +0300

    An interim working version that does not meet all the conditions

--- shirokov.arsenii/U1/main.cpp
+++ shirokov.arsenii/U1/main.cpp
@@ -1,4 +1,11 @@
 #include <cstddef>
+#include <fstream>
+#include <ios>
+#include <iostream>
+#include <istream>
+#include <limits>
+#include <new>
+#include <stdexcept>
 #include <string>
 
 namespace shirokov
@@ -8,7 +15,176 @@ namespace shirokov
     size_t id;
     std::string info;
   };
+
+  void makeIn(const std::string&, std::ifstream&, std::istream**);
+  void makeOut(const std::string&, std::ofstream&, std::ostream**);
+  void expand(Person**, size_t, size_t&);
 }
 
-int main()
-{}
+int main(int argc, char* argv[])
+{
+  std::istream* inPtr = &std::cin;
+  std::ifstream fileIn;
+  std::ostream* outPtr = &std::cout;
+  std::ofstream fileOut;
+
+  if (argc > 1)
+  {
+    std::string filename = argv[1];
+    if (filename.compare(0, 3, "in:") == 0)
+    {
+      try
+      {
+        shirokov::makeIn(filename.substr(3), fileIn, &inPtr);
+      }
+      catch (const std::runtime_error& e)
+      {
+        std::cerr << e.what() << '\n';
+        return 1;
+      }
+    }
+    else if (filename.compare(0, 4, "out:") == 0)
+    {
+      try
+      {
+        shirokov::makeOut(filename.substr(4), fileOut, &outPtr);
+      }
+      catch (const std::runtime_error& e)
+      {
+        std::cerr << e.what() << '\n';
+        return 1;
+      }
+    }
+  }
+
+  if (argc > 2)
+  {
+    std::string filename = argv[2];
+    if (filename.compare(0, 3, "in:") == 0)
+    {
+      try
+      {
+        shirokov::makeIn(filename.substr(3), fileIn, &inPtr);
+      }
+      catch (const std::runtime_error& e)
+      {
+        std::cerr << e.what() << '\n';
+        return 1;
+      }
+    }
+    else if (filename.compare(0, 4, "out:") == 0)
+    {
+      try
+      {
+        shirokov::makeOut(filename.substr(4), fileOut, &outPtr);
+      }
+      catch (const std::runtime_error& e)
+      {
+        std::cerr << e.what() << '\n';
+        return 1;
+      }
+    }
+  }
+
+  std::ostream& out = *outPtr;
+  std::istream& in = *inPtr;
+
+  shirokov::Person* massive = nullptr;
+  size_t size = 0, cap = 0;
+
+  size_t id = 0;
+  while (in >> id)
+  {
+    in >> std::ws;
+    std::string info;
+
+    if (!std::getline(in, info))
+    {
+      if (!in.eof())
+      {
+        in.clear();
+        in.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+        continue;
+      }
+      break;
+    }
+
+    if (size == cap)
+    {
+      try
+      {
+        shirokov::expand(&massive, size, cap);
+      }
+      catch (const std::bad_alloc&)
+      {
+        std::cerr << "Bad alloc" << '\n';
+        delete[] massive;
+        return 3;
+      }
+    }
+    massive[size++] = shirokov::Person{id, info};
+  }
+
+  if (!in && !in.eof())
+  {
+    std::cerr << "Input format error occurred.\n";
+  }
+
+  for (size_t i = 0; i < size; ++i)
+  {
+    out << massive[i].id << ' ' << massive[i].info << '\n';
+  }
+
+  delete[] massive;
+}
+
+void shirokov::makeIn(const std::string& filename, std::ifstream& fileIn, std::istream** inPtr)
+{
+  if (fileIn.is_open())
+  {
+    fileIn.close();
+    fileIn.clear();
+  }
+  fileIn.open(filename);
+  if (fileIn.is_open())
+  {
+    *inPtr = &fileIn;
+  }
+  else
+  {
+    throw std::runtime_error("Couldn't open the input file: " + filename);
+  }
+}
+
+void shirokov::makeOut(const std::string& filename, std::ofstream& fileOut, std::ostream** outPtr)
+{
+  if (fileOut.is_open())
+  {
+    fileOut.close();
+    fileOut.clear();
+  }
+  fileOut.open(filename);
+  if (fileOut.is_open())
+  {
+    *outPtr = &fileOut;
+  }
+  else
+  {
+    throw std::runtime_error("Couldn't open the output file: " + filename);
+  }
+}
+
+void shirokov::expand(Person** massive, size_t size, size_t& cap)
+{
+  size_t newCap = (cap == 0 ? 1 : cap * 2);
+  Person* newMassive = new Person[newCap];
+
+  for (size_t i = 0; i < size; ++i)
+  {
+    newMassive[i] = (*massive)[i];
+  }
+
+  delete[] *massive;
+  *massive = newMassive;
+  cap = newCap;
+}
