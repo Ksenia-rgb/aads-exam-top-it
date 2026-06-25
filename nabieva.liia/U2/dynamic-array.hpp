6c470a01991bd0e2a4891723b15a4bb0f26a31a0
liia <kirilich_lubit@rambler.ru>
2026-06-25 13:14:21 +0300

    init dynamic-array

---
+++ nabieva.liia/U2/dynamic-array.hpp
@@ -0,0 +1,26 @@
+#ifndef KIRILICH_DYNAMIC_ARRAY_HPP
+#define KIRILICH_DYNAMIC_ARRAY_HPP
+
+#include <cstddef>
+#include <stdexcept>
+
+namespace nabieva
+{
+  template< typename T >
+  struct DynamicArray
+  {
+    T* data;
+    std::size_t size;
+    std::size_t capacity;
+  };
+
+  template< typename T >
+  void init(DynamicArray< T >& array)
+  {
+    array.data = 0;
+    array.size = 0;
+    array.capacity = 0;
+  }
+}
+
+#endif
