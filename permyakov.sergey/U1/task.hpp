dbd88761abbae1fa7a27cc1d89bc9eedf3eca6d8
root <staveplay@mail.ru>
2026-06-25 12:08:42 +0300

    implement clear and add methods

--- permyakov.sergey/U1/task.hpp
+++ permyakov.sergey/U1/task.hpp
@@ -18,9 +18,35 @@ namespace permyakov
     list< T > * next;
   };
 
-  template< class T > void clear(list< T > lst);
-  template< class T > list< T > * addAfter(list< T > lst);
-  template< class T > void addToEnd(list< T > lst);
+  template< class T > void clear(list< T > lst)
+  {
+    list< T > * tmpLst = lst;
+    while (tmpLst->next) {
+      list< T > * prv = tmpLst;
+      tmpLlst = rmpLst->next;
+      delete prv;
+    }
+    delete tmpLst;
+  }
+
+  template< class T > list< T > * addAfter(list< T > & lst, const T val)
+  {
+    list< T > * nwNode = new list< T >();
+    nwNode->data = val;
+    nwNode->next = lst->next;
+    lst->next = nwNode;
+    return nwNode;
+  }
+
+  template< class T > void addToEnd(list< T > & lst, const T val)
+  {
+    list< T > * tmpLst = lst;
+    while (tmpLst->next) {
+      tmpLst = tmpLst->next;
+    }
+    addAfter(tmpLst, val);
+  }
+
   std::pair< size_t, size_t > input(std::istream & in, list< Person > & lst);
   void output(std::osream & out, list< Person > & lst);
 }
