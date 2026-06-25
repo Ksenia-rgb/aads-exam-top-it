801983ba2e948985461d8e72e1570c44152e7fa7
grisha-git <donkeevgrisa@gmail.com>
2026-06-25 10:57:08 +0300

    Interface

--- donkeev.grigoriy/U1/main.cpp
+++ donkeev.grigoriy/U1/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+namespace donkeev
+{
+  struct Person
+  {
+    size_t id;
+    std::string info;
+  };
+
+  struct PersonArray
+  {
+    std::pair< size_t, Person* > data;
+    size_t size;
+    size_t capacity;
+  };
+}
 int main()
 {
   
+  
+
+
 }
