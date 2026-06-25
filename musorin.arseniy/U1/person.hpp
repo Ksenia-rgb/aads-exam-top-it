b9e2004cb1435de458c8fb40ff43033cfa1525ad
arseniymusorin@gmail.com <quaka@quaka.localdomain>
2026-06-25 11:08:23 +0300

    add person

---
+++ musorin.arseniy/U1/person.hpp
@@ -0,0 +1,13 @@
+#ifndef MUSORIN_PERSON_HPP
+#define MUSORIN_PERSON_HPP
+#include <cstddef>
+#include <string>
+namespace musorin
+{
+struct Person
+{
+  size_t id;
+  std::string info;
+};
+}
+#endif
