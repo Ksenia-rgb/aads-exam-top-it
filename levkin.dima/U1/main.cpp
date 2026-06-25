ba5cd11cfd92470e89eebe2665324ce57f702881
Dmitry Levkin <mit.levkin@vk.com>
2026-06-25 11:04:31 +0300

    Parse command line arguments for file paths

--- levkin.dima/U1/main.cpp
+++ levkin.dima/U1/main.cpp
@@ -4,9 +4,24 @@ int main(int argc, char* argv[])
   if (argc > 3) {
     return 1;
   }
-
   std::string in_file;
   std::string out_file;
   bool has_in = false;
   bool has_out = false;
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i];
+    if (arg.rfind("in:", 0) == 0) {
+      if (has_in)
+        return 1;
+      in_file = arg.substr(3);
+      has_in = true;
+    } else if (arg.rfind("out:", 0) == 0) {
+      if (has_out)
+        return 1;
+      out_file = arg.substr(4);
+      has_out = true;
+    } else {
+      return 1;
+    }
+  }
 }
