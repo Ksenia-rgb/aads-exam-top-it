a83d39d279c65f147a22c8c7c975d51835096f48
legleb <sedovgleb2570@gmail.com>
2026-06-25 13:51:42 +0300

    fix main

--- sedov.gleb/U1/main.cpp
+++ sedov.gleb/U1/main.cpp
@@ -87,7 +87,7 @@ int main(int argc, char ** argv)
         * outStream << persons.data[i].id << " " << persons.data[i].info << "\n";
       }
     }
-    std::cerr << persons.size << " " << ignoreCount << "\n";
+    std::cout << persons.size << " " << ignoreCount << "\n";
     cleanupResources(persons, seen);
     return 0;
   }
