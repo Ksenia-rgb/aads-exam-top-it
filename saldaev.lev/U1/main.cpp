539ba35620441e584cc4bfcceeb1e9e13f08f5a9
Carrot_lord <lev.saldaev@gmai.com>
2026-06-25 13:31:52 +0300

    implement reading logic

--- saldaev.lev/U1/main.cpp
+++ saldaev.lev/U1/main.cpp
@@ -3,6 +3,25 @@
 #include <limits>
 #include <string>
 
+struct Person
+{
+  size_t id;
+  std::string info;
+};
+
+namespace saldaev
+{
+  bool IdExcist(Person *persons, size_t s, size_t id)
+  {
+    for (size_t i = 0; i < s; ++i) {
+      if (persons[i].id == id) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
 int main(int argc, char *argv[])
 {
   if (argc > 2) {
@@ -50,4 +69,47 @@ int main(int argc, char *argv[])
 
   std::istream &in = ifl ? ifile : std::cin;
   std::ostream &out = ofl ? ofile : std::cout;
+
+  Person *base = new Person[10];
+  size_t cap = 10;
+  size_t curr = 0;
+  size_t ignored = 0;
+
+  size_t id = 0;
+  std::string info;
+  while (true) {
+    if (in >> id) {
+      if (std::getline(in >> std::ws, info)) {
+        if (saldaev::IdExcist(base, curr, id)) {
+          ++ignored;
+          continue;
+        }
+
+        if (curr == cap) {
+          size_t newcap = cap * 2;
+          Person *newbase = new Person[newcap];
+          for (size_t j = 0; j < curr; ++j) {
+            newbase[j] = base[j];
+          }
+          delete[] base;
+          base = newbase;
+          cap = newcap;
+        }
+        base[curr++] = {id, info};
+        continue;
+      }
+    }
+
+    if (in.eof()) {
+      break;
+    } else {
+      in.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    ++ignored;
+  }
+
+  if (ifl) {
+    ifile.close();
+  }
 }
