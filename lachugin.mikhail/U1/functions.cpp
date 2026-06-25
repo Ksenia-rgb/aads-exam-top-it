d21af43aa7202cb1a7ed94381363618d5c0f05ec
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-06-25 11:17:00 +0300

    add Person and functions

---
+++ lachugin.mikhail/U1/functions.cpp
@@ -0,0 +1,9 @@
+#include "functions.hpp"
+namespace lachugin
+{
+  void pushBack(Person*& persons, size_t& size, size_t& capacity, const Person& person)
+  {
+
+  }
+
+}
---
+++ lachugin.mikhail/U1/functions.hpp
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+#include <iostream>
+#include "Person.hpp"
+namespace lachugin
+{
+  void pushBack(Person*& persons, size_t& size, size_t& capacity, const Person& person);
+  bool containsId(const Person* persons, size_t size, size_t id);
+}
+
+#endif
