8917c18bda58eef92b6432b6c4de5131da2e6b11
L0syash <losev_timofey07@mail.ru>
2026-06-25 11:46:14 +0300

    Added functions for working with the list: adding, searching, clearing

--- losev.timofey/U1/main.cpp
+++ losev.timofey/U1/main.cpp
@@ -14,6 +14,68 @@ namespace losev
     Person data;
     Node* next;
   };
+
+  Node* createNode(const Person& person)
+  {
+    Node* node = new Node;
+    node->data = person;
+    node->next = nullptr;
+    return node;
+  }
+
+  void pushBack(Node*& head, const Person& person)
+  {
+    Node* newNode = createNode(person);
+
+    if (head == nullptr)
+    {
+      head = newNode;
+      return;
+    }
+
+    Node* current = head;
+    while (current->next != nullptr)
+    {
+      current = current->next;
+    }
+    current->next = newNode;
+  }
+
+  bool findId(const Node* head, size_t id)
+  {
+    const Node* current = head;
+    while (current != nullptr)
+    {
+      if (current->data.id == id)
+      {
+        return true;
+      }
+      current = current->next;
+    }
+    return false;
+  }
+
+  void clearList(Node*& head)
+  {
+    while (head != nullptr)
+    {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
+
+  size_t getSize(const Node* head)
+  {
+    size_t count = 0;
+    const Node* current = head;
+    while (current != nullptr)
+    {
+      ++count;
+      current = current->next;
+    }
+    return count;
+  }
 }
 
 int main()
