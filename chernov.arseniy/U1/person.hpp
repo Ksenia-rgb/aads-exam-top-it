9b478298c2c9e35c520e3a25694bc869eb0de161
aaaa24 <chernov.arseniy@yandex.ru>
2026-06-25 11:11:11 +0300

    add findById for Vector< Person >

--- chernov.arseniy/U1/person.hpp
+++ chernov.arseniy/U1/person.hpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include <vector.hpp>
 
 namespace chernov {
 
@@ -10,4 +11,15 @@ namespace chernov {
     std::string info;
   };
 
+  size_t findById(const Vector< Person > & persons, size_t id);
+}
+
+size_t chernov::findById(const Vector< Person > & persons, size_t id)
+{
+  for (size_t i = 0; i < persons.size; ++i) {
+    if (persons.data[i].id == id) {
+      return i;
+    }
+  }
+  return persons.size;
 }
