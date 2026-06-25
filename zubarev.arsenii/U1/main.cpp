bec1b38cafce8916b3526aa504d733fa599a93fa
zub <ars1for1work@gmail.com>
2026-06-25 14:13:22 +0300

    fix(main.cpp): reorder and simplify argument parsing logic

--- zubarev.arsenii/U1/main.cpp
+++ zubarev.arsenii/U1/main.cpp
@@ -8,13 +8,12 @@ int main(int argc, char* argv[])
 {
   std::string in_name = "";
   std::string out_name = "";
-
-  if (!zubarev::detail::parse_args(argc, argv, in_name, out_name)) {
-    return 1;
-  }
   if (argc > 3) {
     std::cerr << "Too mane args" << '\n';
     return 0;
+  }
+  if (!zubarev::detail::parse_args(argc, argv, in_name, out_name)) {
+    return 1;
   }
 
   std::istream* input = &std::cin;
@@ -27,17 +26,6 @@ int main(int argc, char* argv[])
     }
     input = std::addressof(in_file);
   }
-
-  // std::ostream* output = &std::cout;
-  // std::ofstream out_file;
-
-  // if (!out_name.empty()) {
-  //   out_file.open(out_name);
-  //   if (!out_file) {
-  //     return 2;
-  //   }
-  //   output = std::addressof(out_file);
-  // }
 
   zubarev::PersonArray persons;
   zubarev::HashSet ids;
