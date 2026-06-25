534f675cf71778432f01869504e67f6a20c00fbc
ArtConion <artiom0408@mail.ru>
2026-06-25 13:08:37 +0300

    read input before writing output to handle same file

--- vishnyakov.artiom/U1/main.cpp
+++ vishnyakov.artiom/U1/main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char* argv[])
 
   if (argc > 3)
   {
-    std::cerr << "Too many arguments\n";
+    std::cerr << "Invalid arguments\n";
     return 0;
   }
 
@@ -49,118 +49,180 @@ int main(int argc, char* argv[])
     }
   }
 
-  std::ifstream inputFileStream;
-  if (!inputFile.empty())
-  {
-    inputFileStream.open(inputFile);
-    if (!inputFileStream.is_open())
-    {
-      std::cerr << "Cannot open input file\n";
-      return 2;
-    }
-  }
-
-  std::istream& in = inputFile.empty() ? std::cin : inputFileStream;
-
-  std::ofstream outputFileStream;
-  if (!outputFile.empty())
-  {
-    outputFileStream.open(outputFile);
-    if (!outputFileStream.is_open())
-    {
-      std::cerr << "Cannot open output file\n";
-      return 2;
-    }
-  }
-
-  std::ostream& out = outputFile.empty() ? std::cout : outputFileStream;
-
   List< Person > persons;
   HashSet< size_t > usedIds;
   size_t successful = 0;
   size_t ignored = 0;
   std::string line;
 
-  while (std::getline(in, line))
-  {
-
-    size_t first = line.find_first_not_of(" \t");
-    if (first == std::string::npos)
-    {
-      continue;
-    }
-
-    size_t space = line.find_first_of(" \t", first);
-    if (space == std::string::npos)
-    {
-      ++ignored;
-      continue;
-    }
-
-    std::string idStr = line.substr(first, space - first);
-    size_t descStart = line.find_first_not_of(" \t", space);
-    if (descStart == std::string::npos)
-    {
-      ++ignored;
-      continue;
-    }
-
-    std::string info = line.substr(descStart);
-    if (info.find_first_not_of(" \t") == std::string::npos)
-    {
-      ++ignored;
-      continue;
-    }
-
-    size_t id = 0;
-    bool validId = true;
-    for (size_t i = 0; i < idStr.length(); ++i)
-    {
-      if (!std::isdigit(idStr[i]))
-      {
-        validId = false;
-        break;
-      }
-      id = id * 10 + (idStr[i] - '0');
-    }
-
-    if (!validId)
-    {
-      ++ignored;
-      continue;
-    }
-
-    if (usedIds.has(id))
-    {
-      ++ignored;
-      continue;
-    }
-
-    Person p;
-    p.id = id;
-    p.info = info;
-    persons.pushBack(p);
-    usedIds.insert(id);
-    ++successful;
-  }
-
   if (!inputFile.empty())
   {
-    inputFileStream.close();
+    std::ifstream in(inputFile);
+    if (!in.is_open())
+    {
+      std::cerr << "Cannot open file\n";
+      return 2;
+    }
+
+    while (std::getline(in, line))
+    {
+      size_t first = line.find_first_not_of(" \t");
+      if (first == std::string::npos)
+      {
+        continue;
+      }
+
+      size_t space = line.find_first_of(" \t", first);
+      if (space == std::string::npos)
+      {
+        ++ignored;
+        continue;
+      }
+
+      std::string idStr = line.substr(first, space - first);
+      size_t descStart = line.find_first_not_of(" \t", space);
+      if (descStart == std::string::npos)
+      {
+        ++ignored;
+        continue;
+      }
+
+      std::string info = line.substr(descStart);
+      if (info.find_first_not_of(" \t") == std::string::npos)
+      {
+        ++ignored;
+        continue;
+      }
+
+      size_t id = 0;
+      bool validId = true;
+      for (size_t i = 0; i < idStr.length(); ++i)
+      {
+        if (!std::isdigit(idStr[i]))
+        {
+          validId = false;
+          break;
+        }
+        id = id * 10 + (idStr[i] - '0');
+      }
+
+      if (!validId)
+      {
+        ++ignored;
+        continue;
+      }
+
+      if (usedIds.has(id))
+      {
+        ++ignored;
+        continue;
+      }
+
+      Person p;
+      p.id = id;
+      p.info = info;
+      persons.pushBack(p);
+      usedIds.insert(id);
+      ++successful;
+    }
+  }
+  else
+  {
+    while (std::getline(std::cin, line))
+    {
+      size_t first = line.find_first_not_of(" \t");
+      if (first == std::string::npos)
+      {
+        continue;
+      }
+
+      size_t space = line.find_first_of(" \t", first);
+      if (space == std::string::npos)
+      {
+        ++ignored;
+        continue;
+      }
+
+      std::string idStr = line.substr(first, space - first);
+      size_t descStart = line.find_first_not_of(" \t", space);
+      if (descStart == std::string::npos)
+      {
+        ++ignored;
+        continue;
+      }
+
+      std::string info = line.substr(descStart);
+      if (info.find_first_not_of(" \t") == std::string::npos)
+      {
+        ++ignored;
+        continue;
+      }
+
+      size_t id = 0;
+      bool validId = true;
+      for (size_t i = 0; i < idStr.length(); ++i)
+      {
+        if (!std::isdigit(idStr[i]))
+        {
+          validId = false;
+          break;
+        }
+        id = id * 10 + (idStr[i] - '0');
+      }
+
+      if (!validId)
+      {
+        ++ignored;
+        continue;
+      }
+
+      if (usedIds.has(id))
+      {
+        ++ignored;
+        continue;
+      }
+
+      Person p;
+      p.id = id;
+      p.info = info;
+      persons.pushBack(p);
+      usedIds.insert(id);
+      ++successful;
+    }
   }
 
-  if (persons.size() > 0)
+  if (!outputFile.empty())
   {
-    ListNode< Person >* current = persons.begin();
-    out << current->data.id << " " << current->data.info;
-    current = current->next;
-
-    while (current != nullptr)
+    std::ofstream out(outputFile);
+    if (!out.is_open())
     {
-      out << "\n" << current->data.id << " " << current->data.info;
-      current = current->next;
+      std::cerr << "Cannot open file\n";
+      return 2;
     }
-    out << "\n";
+
+    for (ListNode< Person >* current = persons.begin();
+         current != persons.end();
+         current = current->next)
+    {
+      out << current->data.id << " " << current->data.info << "\n";
+    }
+
+    std::cout << "in file " << outputFile << "\n";
+    for (ListNode< Person >* current = persons.begin();
+         current != persons.end();
+         current = current->next)
+    {
+      std::cout << current->data.id << " " << current->data.info << "\n";
+    }
+  }
+  else
+  {
+    for (ListNode< Person >* current = persons.begin();
+         current != persons.end();
+         current = current->next)
+    {
+      std::cout << current->data.id << " " << current->data.info << "\n";
+    }
   }
 
   std::cerr << successful << " " << ignored << "\n";
