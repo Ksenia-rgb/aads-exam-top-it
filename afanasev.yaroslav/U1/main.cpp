f245cb77195941b09a7c34483683e3bb127f3cf8
jar <Jarafan3@gmail.com>
2026-06-25 11:39:58 +0300

    Add get arguments

--- afanasev.yaroslav/U1/main.cpp
+++ afanasev.yaroslav/U1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <sstream>
 
@@ -55,15 +56,67 @@ bool existsPerson(const Person * persons, size_t count, size_t id)
   return false;
 }
 
-int main()
+int main(int argc, char * argv[])
 {
-  Person* persons = nullptr;
+  if (argc > 3)
+  {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+
+  std::string inFile, outFile;
+  bool hasIn = false, hasOut = false;
+
+  for (int i = 1; i < argc; ++i)
+  {
+    std::string arg = argv[i];
+    if (arg.rfind("in:", 0) == 0)
+    {
+      if (hasIn)
+      {
+        std::cerr << "Duplicate input argument\n";
+        return 1;
+      }
+      hasIn = true;
+      inFile = arg.substr(3);
+    }
+    else if (arg.rfind("out:", 0) == 0)
+    {
+      if (hasOut)
+      {
+        std::cerr << "Duplicate output argument\n";
+        return 1;
+      }
+      hasOut = true;
+      outFile = arg.substr(4);
+    }
+    else
+    {
+      std::cerr << "Invalid argument\n";
+      return 1;
+    }
+  }
+
+  std::ifstream inStream;
+  std::istream * in = &std::cin;
+  if (hasIn)
+  {
+    inStream.open(inFile);
+    if (!inStream.is_open())
+    {
+      std::cerr << "Cannot open input file\n";
+      return 2;
+    }
+    in = &inStream;
+  }
+
+  Person * persons = nullptr;
   size_t count = 0;
   size_t capacity = 0;
   size_t ignored = 0;
   std::string line;
 
-  while (std::getline(std::cin, line))
+  while (std::getline(*in, line))
   {
     Person person;
     if (parseLine(line, person))
@@ -83,9 +136,28 @@ int main()
     }
   }
 
+  if (hasIn)
+  {
+    inStream.close();
+  }
+
+  std::ofstream outStream;
+  std::ostream * out = &std::cout;
+  if (hasOut)
+  {
+    outStream.open(outFile);
+    if (!outStream.is_open())
+    {
+      std::cerr << "Cannot open output file\n";
+      delete[] persons;
+      return 2;
+    }
+    out = &outStream;
+  }
+
   for (size_t i = 0; i < count; ++i)
   {
-    std::cout << persons[i].id << ' ' << persons[i].info << '\n';
+    *out << persons[i].id << ' ' << persons[i].info << '\n';
   }
 
   std::cerr << count << ' ' << ignored << '\n';
