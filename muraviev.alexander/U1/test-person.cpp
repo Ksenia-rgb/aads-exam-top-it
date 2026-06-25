35eacb68cb4f81e472594fe43b13626b52d9a011
a1dram <aldram54@gmail.com>
2026-06-25 12:09:35 +0300

    added person simple test

---
+++ muraviev.alexander/U1/test-person.cpp
@@ -0,0 +1,13 @@
+#include "person.hpp"
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(parse_valid_line)
+{
+  muraviev::Person person;
+  const bool result = muraviev::parsePersonLine("42 person info", person);
+
+  BOOST_TEST(result);
+  BOOST_TEST(person.id == 42U);
+  BOOST_TEST(person.info == " person info");
+}
