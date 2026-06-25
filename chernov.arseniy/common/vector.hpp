454dd48b91b902216fd01b57915d1cd9c22e78d9
aaaa24 <chernov.arseniy@yandex.ru>
2026-06-25 10:53:06 +0300

    add init and destroy for Vector

--- chernov.arseniy/common/vector.hpp
+++ chernov.arseniy/common/vector.hpp
@@ -7,4 +7,27 @@ namespace chernov {
     size_t capacity;
   };
 
+  template< class T >
+  void init(Vector< T > & v);
+
+  template< class T >
+  void destroy(Vector< T > & v);
+
+}
+
+template< typename T >
+void chernov::init(Vector< T > & v)
+{
+  v.data = nullptr;
+  v.size = 0;
+  v.capacity = 0;
+}
+
+template< typename T >
+void chernov::destroy(Vector< T > & v)
+{
+  if (v.data) {
+    delete[] v.data;
+  }
+  init(v);
 }
