e6e214c8e14d81ea1c5f287ab7b691f79331fc24
s-sadovnik <sadovnik.sofi@gmail.com>
2026-06-25 12:42:49 +0300

    wire  main to in/out args and add io tests

--- sadovnik.sofi/U1/main.cpp
+++ sadovnik.sofi/U1/main.cpp
@@ -1,13 +1,53 @@
+#include "args.hpp"
 #include "io.hpp"
 
+#include <fstream>
 #include <iostream>
 
-int main()
+int main(int argc, char * argv[])
 {
   using namespace sadovnik;
 
-  const persons_data_t data = readPersons(std::cin);
-  writePersons(std::cout, data.persons);
+  paths_t paths;
+  const int parse_code = parsePaths(argc, argv, paths);
+  if (parse_code != 0)
+  {
+    return parse_code;
+  }
+
+  std::ifstream in_file;
+  std::istream * in = &std::cin;
+  if (paths.use_in_file)
+  {
+    in_file.open(paths.in_file);
+    if (!in_file)
+    {
+      std::cerr << "cannot open input file\n";
+      return 2;
+    }
+    in = &in_file;
+  }
+
+  const persons_data_t data = readPersons(*in);
+  if (paths.use_in_file)
+  {
+    in_file.close();
+  }
+
+  std::ofstream out_file;
+  std::ostream * out = &std::cout;
+  if (paths.use_out_file)
+  {
+    out_file.open(paths.out_file);
+    if (!out_file)
+    {
+      std::cerr << "cannot open output file\n";
+      return 2;
+    }
+    out = &out_file;
+  }
+
+  writePersons(*out, data.persons);
   std::cerr << data.ok_count << ' ' << data.ignored_count << '\n';
   return 0;
 }
---
+++ sadovnik.sofi/U1/test-io.cpp
@@ -0,0 +1,37 @@
+#define BOOST_TEST_MODULE U1_IO
+#include <boost/test/included/unit_test.hpp>
+
+#include "io.hpp"
+
+#include <sstream>
+
+BOOST_AUTO_TEST_CASE(read_persons_from_example)
+{
+  std::istringstream in(
+    "100 Sneaky person\n"
+    "23 Deadman\n"
+    "31 The Agent\n"
+    "56 \"Scarface\"\n"
+    "99\n"
+    "31 Mr. Bond\n");
+
+  const sadovnik::persons_data_t data = sadovnik::readPersons(in);
+
+  BOOST_TEST(data.ok_count == 4);
+  BOOST_TEST(data.ignored_count == 2);
+  BOOST_TEST(data.persons.size() == 4);
+}
+
+BOOST_AUTO_TEST_CASE(write_persons_one_space)
+{
+  sadovnik::List< sadovnik::person_t > persons;
+  sadovnik::person_t person;
+  person.id = 31;
+  person.info = "The Agent";
+  persons.pushBack(person);
+
+  std::ostringstream out;
+  sadovnik::writePersons(out, persons);
+
+  BOOST_TEST(out.str() == "31 The Agent\n");
+}
