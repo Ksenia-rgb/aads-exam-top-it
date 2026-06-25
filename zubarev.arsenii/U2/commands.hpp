4c9a89869b4df771398225ef8615bb84ba8b45d6
zub <ars1for1work@gmail.com>
2026-06-25 13:19:39 +0300

    feat(commands.cpp): add new commands for handling anons, deanon, desc, redesc, meets, commons, less, greater, and out_persons
    refactor(commands.hpp): update function signatures to use AppState instead of PersonTable and include necessary headers

--- zubarev.arsenii/U2/commands.hpp
+++ zubarev.arsenii/U2/commands.hpp
@@ -2,18 +2,18 @@
 #define COMMANDS_HPP
 
 #include <iostream>
-#include "person_table.hpp"
+#include "../common/person.hpp"
 
 namespace zubarev
 {
-  void cmd_anons(std::istream&, std::ostream& out, PersonTable& table);
-  void cmd_deanon(std::istream& in, std::ostream& out, PersonTable& table);
-  void cmd_desc(std::istream& in, std::ostream& out, PersonTable& table);
-  void cmd_redesc(std::istream& in, std::ostream& out, PersonTable& table);
-  void cmd_meets(std::istream& in, std::ostream& out, PersonTable& table);
-  void cmd_commons(std::istream& in, std::ostream& out, PersonTable& table);
-  void cmd_less(std::istream& in, std::ostream& out, PersonTable& table);
-  void cmd_greater(std::istream& in, std::ostream& out, PersonTable& table);
-  void cmd_out_persons(std::istream& in, std::ostream& out, PersonTable& table);
+  void cmd_anons(std::istream&, std::ostream& out, AppState& table);
+  void cmd_deanon(std::istream& in, std::ostream& out, AppState& table);
+  void cmd_desc(std::istream& in, std::ostream& out, AppState& table);
+  void cmd_redesc(std::istream& in, std::ostream& out, AppState& table);
+  void cmd_meets(std::istream& in, std::ostream& out, AppState& table);
+  void cmd_commons(std::istream& in, std::ostream& out, AppState& table);
+  void cmd_less(std::istream& in, std::ostream& out, AppState& table);
+  void cmd_greater(std::istream& in, std::ostream& out, AppState& table);
+  void cmd_out_persons(std::istream& in, std::ostream& out, AppState& table);
 }
 #endif
--- zubarev.arsenii/common/person.hpp
+++ zubarev.arsenii/common/person.hpp
@@ -1,18 +1,36 @@
 #ifndef PERSON_HPP
 #define PERSON_HPP
 
+#include <string>
 #include <iostream>
+
 namespace zubarev
 {
   struct Person
   {
     size_t id;
     std::string info;
+    bool has_info;
+    bool is_deleted;
   };
 
   struct PersonArray
   {
     Person* data;
+    size_t size;
+    size_t capacity;
+  };
+
+  struct Meet
+  {
+    size_t u;
+    size_t v;
+    size_t duration;
+  };
+
+  struct MeetArray
+  {
+    Meet* data;
     size_t size;
     size_t capacity;
   };
@@ -24,18 +42,42 @@ namespace zubarev
     size_t capacity;
   };
 
+  struct AppState
+  {
+    PersonArray persons;
+    MeetArray meets;
+    HashSet ids;
+  };
+
+  struct MeetView
+  {
+    size_t other_id;
+    size_t duration;
+  };
+
+  // Управление динамическим массивом людей
   void init(PersonArray& arr);
   void destroy(PersonArray& arr);
   void resize(PersonArray& arr);
   void push(PersonArray& arr, const Person& value);
-  bool exists(const PersonArray&, size_t);
+  Person* find_person(PersonArray& arr, size_t id);
 
+  // Управление динамическим массивом встреч
+  void init(MeetArray& arr);
+  void destroy(MeetArray& arr);
+  void resize(MeetArray& arr);
+  void push(MeetArray& arr, const Meet& value);
+
+  // Управление Хеш-таблицей (HashSet)
   void init(HashSet& set);
   void destroy(HashSet& set);
   size_t hash(const HashSet& set, size_t value);
   bool contains(const HashSet& set, size_t value);
   void insert(HashSet& set, size_t value);
 
+  // Утилиты сортировки (Вставки)
+  void sort_ids(size_t* arr, size_t size);
+  void sort_meet_views(MeetView* arr, size_t size);
 }
 
 #endif
