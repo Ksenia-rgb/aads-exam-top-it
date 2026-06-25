fc5f494251146a5a9e22726cf4848ed4d82cc0bd
EGGorkaa <egor_novikov20004@mail.ru>
2026-06-25 12:22:11 +0300

    add open files

--- U1/main.cpp
+++ U1/main.cpp
@@ -3,12 +3,12 @@
 #include "List.hpp"
 #include "Person.hpp"
 #include <string>
+#include <fstream>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-  if(argc > 3)
-  {
-    std::cerr<<"Invalid arguments";
+  if (argc > 3) {
+    std::cerr << "Invalid arguments\n";
     return 1;
   }
   std::string inFile;
@@ -16,12 +16,33 @@ int main(int argc, char* argv[])
   for (int i = 1; i < argc; ++i) {
     std::string arg = argv[i];
     if (arg.substr(0, 3) == "in:") {
-        inFile = arg.substr(3);
+      inFile = arg.substr(3);
     } else if (arg.substr(0, 4) == "out:") {
-        outFile = arg.substr(4);
+      outFile = arg.substr(4);
     } else {
-        std::cerr << "Invalid arguments\n";
-        return 1;
+      std::cerr << "Invalid arguments\n";
+      return 1;
     }
+  }
+  std::ifstream inFileStream;
+  std::istream *in = &std::cin;
+  if (!inFile.empty()) {
+    inFileStream.open(inFile);
+    if (!inFileStream) {
+      std::cerr << "Failed to open input file\n";
+      return 2;
+    }
+    in = &inFileStream;
+  }
+
+  std::ofstream outFileStream;
+  std::ostream *out = &std::cout;
+  if (!outFile.empty()) {
+    outFileStream.open(outFile);
+    if (!outFileStream) {
+      std::cerr << "Failed to open output file\n";
+      return 2;
+    }
+    out = &outFileStream;
   }
 }
