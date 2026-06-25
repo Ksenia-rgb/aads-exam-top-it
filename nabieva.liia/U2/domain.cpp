1242930d5e4d90f982e86fa4f1094800477f737d
liia <kirilich_lubit@rambler.ru>
2026-06-25 14:09:42 +0300

    add setDescription

--- nabieva.liia/U2/domain.cpp
+++ nabieva.liia/U2/domain.cpp
@@ -1,4 +1,5 @@
 #include "domain.hpp"
+#include "dynamic-array.hpp"
 
 namespace nabieva
 {
@@ -12,5 +13,36 @@ namespace nabieva
   {
     clear(database.meetings);
     clear(database.persons);
+  }
+
+  int findPersonIndex(const Database& database, int id)
+  {
+    for (std::size_t i = 0; i < database.persons.size; ++i)
+    {
+      if (database.persons.data[i].id == id)
+      {
+        return static_cast<int>(i);
+      }
+    }
+    return -1;
+  }
+
+  Person& getOrCreatePerson(Database& database, int id)
+  {
+    const int index = findPersonIndex(database, id);
+    if (index != -1)
+    {
+      return database.persons.data[index];
+    }
+
+    pushBack(database.persons, makeAnonPerson(id));
+    return database.persons.data[database.persons.size - 1];
+  }
+
+  void setDescription(Database& database, int id, const std::string& description)
+  {
+    Person& person = getOrCreatePerson(database, id);
+    person.description = description;
+    person.hasDescription = true;
   }
 }
