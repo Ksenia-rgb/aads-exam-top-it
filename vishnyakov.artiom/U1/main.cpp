8ab6547218eca25447793c278940bc7603ac31d0
ArtConion <artiom0408@mail.ru>
2026-06-25 12:12:15 +0300

    fix output for acceptance tests

--- vishnyakov.artiom/U1/main.cpp
+++ vishnyakov.artiom/U1/main.cpp
@@ -9,9 +9,16 @@
 
 int main(int argc, char* argv[])
 {
+  using namespace vishnyakov;
+
+  if (argc > 3)
+  {
+    std::cerr << "Too many arguments\n";
+    return 0;
+  }
+
   std::string inputFile;
   std::string outputFile;
-  bool hasInputArg = false;
 
   for (int i = 1; i < argc; ++i)
   {
@@ -21,102 +28,105 @@ int main(int argc, char* argv[])
     {
       if (!inputFile.empty())
       {
-        std::cerr << "Error: duplicate input file argument\n";
+        std::cerr << "Invalid arguments\n";
         return 1;
       }
       inputFile = arg.substr(3);
-      hasInputArg = true;
     }
     else if (arg.rfind("out:", 0) == 0)
     {
       if (!outputFile.empty())
       {
-        std::cerr << "Error: duplicate output file argument\n";
+        std::cerr << "Invalid arguments\n";
         return 1;
       }
       outputFile = arg.substr(4);
     }
     else
     {
-      std::cerr << "Error: invalid argument\n";
+      std::cerr << "Invalid arguments\n";
       return 1;
     }
   }
 
-  std::istream* in = &std::cin;
   std::ifstream inputFileStream;
-
   if (!inputFile.empty())
   {
     inputFileStream.open(inputFile);
     if (!inputFileStream.is_open())
     {
-      std::cerr << "Error: cannot open input file\n";
+      std::cerr << "Cannot open input file\n";
       return 2;
     }
-    in = &inputFileStream;
   }
 
-  std::ostream* out = &std::cout;
-  std::ofstream outputFileStream;
+  std::istream& in = inputFile.empty() ? std::cin : inputFileStream;
 
+  std::ofstream outputFileStream;
   if (!outputFile.empty())
   {
     outputFileStream.open(outputFile);
     if (!outputFileStream.is_open())
     {
-      std::cerr << "Error: cannot open output file\n";
+      std::cerr << "Cannot open output file\n";
       return 2;
     }
-    out = &outputFileStream;
   }
 
-  vishnyakov::List< vishnyakov::Person > persons;
-  vishnyakov::HashSet< size_t > usedIds;
+  std::ostream& out = outputFile.empty() ? std::cout : outputFileStream;
+
+  List< Person > persons;
+  HashSet< size_t > usedIds;
   size_t successful = 0;
   size_t ignored = 0;
   std::string line;
   bool hasData = false;
 
-  while (std::getline(*in, line))
+  while (std::getline(in, line))
   {
     hasData = true;
-    std::string trimmed = vishnyakov::trim(line);
 
-    if (trimmed.empty())
+    size_t first = line.find_first_not_of(" \t");
+    if (first == std::string::npos)
     {
       continue;
     }
 
-    size_t pos = 0;
+    size_t space = line.find_first_of(" \t", first);
+    if (space == std::string::npos)
+    {
+      ++ignored;
+      continue;
+    }
+
+    std::string idStr = line.substr(first, space - first);
+    size_t descStart = line.find_first_not_of(" \t", space);
+    if (descStart == std::string::npos)
+    {
+      ++ignored;
+      continue;
+    }
+
+    std::string info = line.substr(descStart);
+    if (info.find_first_not_of(" \t") == std::string::npos)
+    {
+      ++ignored;
+      continue;
+    }
+
     size_t id = 0;
-
-    while (pos < trimmed.length() && std::isdigit(trimmed[pos]))
+    bool validId = true;
+    for (size_t i = 0; i < idStr.length(); ++i)
     {
-      id = id * 10 + (trimmed[pos] - '0');
-      ++pos;
+      if (!std::isdigit(idStr[i]))
+      {
+        validId = false;
+        break;
+      }
+      id = id * 10 + (idStr[i] - '0');
     }
 
-    if (pos == 0)
-    {
-      ++ignored;
-      continue;
-    }
-
-    while (pos < trimmed.length() && std::isspace(trimmed[pos]))
-    {
-      ++pos;
-    }
-
-    if (pos >= trimmed.length())
-    {
-      ++ignored;
-      continue;
-    }
-
-    std::string info = trimmed.substr(pos);
-
-    if (info.empty())
+    if (!validId)
     {
       ++ignored;
       continue;
@@ -128,7 +138,7 @@ int main(int argc, char* argv[])
       continue;
     }
 
-    vishnyakov::Person p;
+    Person p;
     p.id = id;
     p.info = info;
     persons.pushBack(p);
@@ -136,17 +146,26 @@ int main(int argc, char* argv[])
     ++successful;
   }
 
-  if (hasData || hasInputArg)
+  if (!inputFile.empty())
   {
-    std::cerr << successful << " " << ignored << "\n";
+    inputFileStream.close();
   }
 
-  for (vishnyakov::ListNode< vishnyakov::Person >* current = persons.begin();
-       current != persons.end();
-       current = current->next)
+  if (persons.size() > 0)
   {
-    *out << current->data.id << " " << current->data.info << "\n";
+    ListNode< Person >* current = persons.begin();
+    out << current->data.id << " " << current->data.info;
+    current = current->next;
+
+    while (current != nullptr)
+    {
+      out << "\n" << current->data.id << " " << current->data.info;
+      current = current->next;
+    }
+    out << "\n";
   }
+
+  std::cerr << successful << " " << ignored << "\n";
 
   return 0;
 }
