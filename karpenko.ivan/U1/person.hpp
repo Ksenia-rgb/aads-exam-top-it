3b34f5571712b58ab81b60a18063a5f8c5cd46c7
restorte <vanyakarpenko837@yandex.ru>
2026-06-25 11:07:51 +0300

    add person struct + node

---
+++ karpenko.ivan/U1/person.hpp
@@ -0,0 +1,27 @@
+#ifndef CORE_H
+#define CORE_H
+
+#include <string>
+#include <iosfwd>
+
+namespace karpenko {
+
+    struct Person {
+        size_t id;
+        std::string info;
+    };
+
+    struct Node {
+        Person data;
+        Node* next;
+    };
+
+    bool addPerson(Node*& head, Node*& tail, const Person& p);
+    void clearList(Node*& head);
+    bool parseLine(const std::string& line, Person& out);
+    void printList(const Node* head, std::ostream& out);
+    void printStats(size_t accepted, size_t ignored, std::ostream& err);
+
+}
+
+#endif
