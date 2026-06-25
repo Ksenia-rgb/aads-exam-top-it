8f3da2a3812bbe111d609c72a8007a17d0ece03c
R0me0Star <eip_spb@mail.ru>
2026-06-25 11:25:13 +0300

    arg call in main, base data

--- pozdnyakov.egor/U1/main.cpp
+++ pozdnyakov.egor/U1/main.cpp
@@ -1,2 +1,19 @@
-int main()
-{}
+#include <fstream>
+#include <iostream>
+#include "logic.hpp"
+
+int main(const int argc, char *argv[])
+{
+  const pozdnyakov::AppArgs args = pozdnyakov::parseArguments(argc, argv);
+  
+  pozdnyakov::DynamicArray< pozdnyakov::Person > persons;
+  pozdnyakov::initArray(persons);
+
+  pozdnyakov::ProcessStats stats;
+  stats.successCount = 0;
+  stats.ignoredCount = 0;
+
+  pozdnyakov::destroyArray(persons);
+
+  return 0;
+}
