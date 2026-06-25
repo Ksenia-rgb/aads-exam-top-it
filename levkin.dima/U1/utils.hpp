9bb09244c110fe7307537d5b273b94a9de9c2cc3
Dmitry Levkin <mit.levkin@vk.com>
2026-06-25 11:03:02 +0300

    Add Pair alias to utils header

--- levkin.dima/U1/utils.hpp
+++ levkin.dima/U1/utils.hpp
@@ -1,4 +1,14 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <cstddef>
+#include <iosfwd>
+#include <string.h>
+#include <utility>
+
+namespace levkin {
+  using Pair = std::pair< size_t, std::string >;
+  
+}
+
 #endif
