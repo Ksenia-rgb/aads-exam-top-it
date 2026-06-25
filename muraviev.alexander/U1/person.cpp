e2070335909481c04558d03726fdd01e34f98e41
a1dram <aldram54@gmail.com>
2026-06-25 12:08:27 +0300

    parser reads digit line

---
+++ muraviev.alexander/U1/person.cpp
@@ -0,0 +1,24 @@
+#include "person.hpp"
+
+namespace muraviev
+{
+  bool parsePersonLine(const std::string& line, Person& person)
+  {
+    if (line.empty() || line[0] < '0' || line[0] > '9')
+    {
+      return false;
+    }
+    size_t position = 0;
+    size_t id = 0;
+    while (position < line.size() && line[position] >= '0' &&
+        line[position] <= '9')
+    {
+      const size_t digit = line[position] - '0';
+      id = id * 10 + digit;
+      ++position;
+    }
+    person.id = id;
+    person.info = line.substr(position);
+    return true;
+  }
+}
