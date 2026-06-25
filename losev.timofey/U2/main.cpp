078d6f86a20a690892b2da315d9ec6cf70b08860
L0syash <losev_timofey07@mail.ru>
2026-06-25 13:47:29 +0300

    Add data structures for storing people and meetings

--- losev.timofey/U2/main.cpp
+++ losev.timofey/U2/main.cpp
@@ -1,20 +1,51 @@
 #include <iostream>
+#include <fstream>
 #include <string>
-#include <vector>
 #include <stdexcept>
 
 #include "../common/person.hpp"
 #include "meeting.hpp"
 #include "storage.hpp"
 #include "args.hpp"
+#include "io.hpp"
 
 int main(int argc, char* argv[])
 {
   try
   {
     losev::Args args = losev::parseArgs(argc, argv);
-    std::cout << "in: " << (args.hasIn ? args.inFile : "(stdin)") << "\n";
-    std::cout << "data: " << args.dataFile << "\n";
+
+    // Читаем людей
+    losev::PersonStorage people;
+    initPersonStorage(people);
+
+    if (args.hasIn)
+    {
+      std::ifstream inFile(args.inFile);
+      if (!inFile.is_open())
+      {
+        std::cerr << "Failed to open input file\n";
+        return 2;
+      }
+      losev::readPeople(inFile, people);
+    }
+
+    losev::MeetingStorage meetings;
+    initMeetingStorage(meetings);
+
+    std::ifstream dataFile(args.dataFile);
+    if (!dataFile.is_open())
+    {
+      std::cerr << "Failed to open data file\n";
+      clearPersonStorage(people);
+      return 2;
+    }
+    losev::readMeetings(dataFile, meetings);
+
+
+    clearPersonStorage(people);
+    clearMeetingStorage(meetings);
+
     return 0;
   }
   catch (const std::invalid_argument& e)
--- losev.timofey/U2/storage.hpp
+++ losev.timofey/U2/storage.hpp
@@ -1,21 +1,43 @@
 #ifndef LOSEV_STORAGE_HPP
 #define LOSEV_STORAGE_HPP
 
-#include <vector>
 #include "../common/person.hpp"
 #include "meeting.hpp"
 
 namespace losev
 {
+  struct PersonNode
+  {
+    Person data;
+    PersonNode* next;
+  };
+
+  struct MeetingNode
+  {
+    Meeting data;
+    MeetingNode* next;
+  };
+
   struct PersonStorage
   {
-    std::vector<Person> people;
+    PersonNode* head;
+    PersonNode* tail;
   };
 
   struct MeetingStorage
   {
-    std::vector<Meeting> meetings;
+    MeetingNode* head;
+    MeetingNode* tail;
   };
+
+  void initPersonStorage(PersonStorage& storage);
+  void addPerson(PersonStorage& storage, const Person& person);
+  bool findPersonId(const PersonStorage& storage, size_t id);
+  void clearPersonStorage(PersonStorage& storage);
+
+  void initMeetingStorage(MeetingStorage& storage);
+  void addMeeting(MeetingStorage& storage, const Meeting& meeting);
+  void clearMeetingStorage(MeetingStorage& storage);
 }
 
 #endif
