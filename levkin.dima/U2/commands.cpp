3fc2af5a37d1f72bce5f3cb4e2e1329c1349f3e7
Dmitry Levkin <mit.levkin@vk.com>
2026-06-25 13:19:51 +0300

    Implement desc command for person descriptions

--- levkin.dima/U2/commands.cpp
+++ levkin.dima/U2/commands.cpp
@@ -1,5 +1,6 @@
 #include "commands.hpp"
 #include "utils.hpp"
+#include "db.hpp"
 
 namespace levkin {
   void anons(const DB& db, std::ostream& os)
@@ -17,5 +18,24 @@ namespace levkin {
       os << anon_ids.data[i] << "\n";
     }
     freeVec(anon_ids);
+  }
+
+  void desc(const DB& db, std::istream& is, std::ostream& os)
+  {
+    size_t id;
+    if (!(is >> id) || !hasPerson(db, id)) {
+      os << "<INVALID COMMAND>\n";
+      is.clear();
+      return;
+    }
+    for (size_t i = 0; i < db.persons.size; ++i) {
+      if (db.persons.data[i].first == id) {
+        if (db.persons.data[i].second.empty())
+          os << "<NO DESCRIPTION>\n";
+        else
+          os << db.persons.data[i].second << "\n";
+        break;
+      }
+    }
   }
 }
