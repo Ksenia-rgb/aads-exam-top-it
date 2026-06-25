f5d3eed0682521d6a88fef80a95c66f22f1efd89
kirusha-13 <kirillukashevich2007@gmail.com>
2026-06-25 11:03:34 +0300

    add initArray

--- lukashevich.kirill/U1/array.hpp
+++ lukashevich.kirill/U1/array.hpp
@@ -5,13 +5,29 @@
 
 namespace lukashevich
 {
-  template< typename T >
+  namespace detail
+  {
+    const size_t INITIAL_ARRAY_CAPACITY = 4;
+  }
+
+  template< class T >
   struct Array
   {
     T* data_;
     size_t size_;
     size_t capacity_;
   };
+
+  template< class T >
+  void initArray(Array< T >& array) noexcept
+  {
+    array.data = nullptr;
+    array.size_ = 0;
+    array.capacity_ = 0;
+  }
+
+  template< class T >
+  void destroyArray(Array< T >& array) noexcept
 }
 
 #endif
