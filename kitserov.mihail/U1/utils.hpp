eeda1afb2f0d9a101be999883ae60eccae4cf055
Kreniker <kitserov.mike@gmail.com>
2026-06-25 11:23:29 +0300

    read line for id and info

--- kitserov.mihail/U1/utils.hpp
+++ kitserov.mihail/U1/utils.hpp
@@ -12,17 +12,35 @@ namespace kitserov
       out << container.data[i].id << ' ' << container.data[i].info << '\n';
     }
   }
-  bool readLine(const std::string& line, std::size_t& id, std::string& info)
+  bool readLine(const std::string& line, size_t& id, std::string& info)
   {
     size_t pos = 0;
-    const std::size_t len = line.size();
-    while (pos < len && (line[pos] == ' ' || line[pos] == '\t')) {
-        ++pos;
+    const size_t len = line.size();
+    while (pos < len && line[pos] == ' ') {
+      ++pos;
     }
     if (pos == len) {
-        return false;
+      return false;
     }
-
+    id = 0;
+    bool hasDigit = false;
+    while (pos < len && std::isdigit(static_cast< unsigned char >(line[pos]))) {
+      hasDigit = true;
+      id = id * 10 + static_cast< size_t >(line[pos] - '0');
+      ++pos;
+    }
+    if (!hasDigit) {
+      return false;
+    }
+    while (pos < len && line[pos] == ' ') {
+      ++pos;
+    }
+    if (pos < len) {
+      info = line.substr(pos);
+    } else {
+      info.clear();
+    }
+    return true;
   }
 }
 
