7bea854f41036630223e630e29d19642f1fece00
Владислав  Шигарев <vladislavsigarev@MBPWiFi.local>
2026-06-25 12:35:31 +0300

    Some minor fixes

--- shigarev.vladislav/U1/personio.cpp
+++ shigarev.vladislav/U1/personio.cpp
@@ -132,5 +132,8 @@ void shigarev::writePersons(std::ostream& out,
 void shigarev::writeStats(std::ostream& err,
     size_t read, size_t ignored)
 {
+  if (read == 0 && ignored == 0) {
+    return;
+  }
   err << read << " " << ignored << "\n";
 }
