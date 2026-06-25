a86c6400d60cbb5b69bfc5d257b665c2fc662189
a1dram <aldram54@gmail.com>
2026-06-25 12:34:55 +0300

    added stream choise in main

--- muraviev.alexander/U1/main.cpp
+++ muraviev.alexander/U1/main.cpp
@@ -1,6 +1,7 @@
 #include "options.hpp"
 #include "person.hpp"
 
+#include <fstream>
 #include <iostream>
 
 int main(int argc, char** argv)
@@ -11,12 +12,36 @@ int main(int argc, char** argv)
   {
     return 1;
   }
+
+  std::ifstream inputFile;
+  std::ofstream outputFile;
+  std::istream* input = &std::cin;
+  std::ostream* output = &std::cout;
+  if (options.hasInput)
+  {
+    inputFile.open(options.inputName);
+    if (!inputFile)
+    {
+      return 2;
+    }
+    input = &inputFile;
+  }
+  if (options.hasOutput)
+  {
+    outputFile.open(options.outputName);
+    if (!outputFile)
+    {
+      return 2;
+    }
+    output = &outputFile;
+  }
+
   muraviev::PersonList persons;
   muraviev::initList(persons);
   size_t accepted = 0;
   size_t ignored = 0;
-  muraviev::readPersons(std::cin, persons, accepted, ignored);
-  muraviev::printPersons(std::cout, persons);
+  muraviev::readPersons(*input, persons, accepted, ignored);
+  muraviev::printPersons(*output, persons);
   std::cerr << accepted << ' ' << ignored << '\n';
   muraviev::clearList(persons);
   return 0;
