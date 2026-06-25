136a436ab9f3ef9a34e2c571d0b85ffefe6b2127
StKondrat <kndrt13@icloud.com>
2026-06-25 12:53:37 +0300

    add parseArgs

--- kondrat.stepan/U2/main.cpp
+++ kondrat.stepan/U2/main.cpp
@@ -1,5 +1,7 @@
 #include <cstddef>
+#include <iostream>
 #include <string>
+#include <utils.hpp>
 
 namespace kondrat
 {
@@ -36,7 +38,45 @@ namespace kondrat
     const char * input;
     const char * data;
   };
+
+  bool parseArgs(int argc, char ** argv, ProgramArgs & args);
 }
 
-int main()
+bool kondrat::parseArgs(int argc, char ** argv, ProgramArgs & args)
+{
+  args.input = nullptr;
+  args.data = nullptr;
+  if (argc < 2 || argc > 3)
+  {
+    return false;
+  }
+
+  for (int i = 1; i < argc; ++i)
+  {
+    if (startsWith(argv[i], "in:"))
+    {
+      if (args.input != nullptr || argv[i][3] == '\0')
+      {
+        return false;
+      }
+      args.input = argv[i] + 3;
+    }
+    else if (startsWith(argv[i], "data:"))
+    {
+      if (args.data != nullptr || argv[i][5] == '\0')
+      {
+        return false;
+      }
+      args.data = argv[i] + 5;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  return args.data != nullptr;
+}
+
+int main(int argc, char ** argv)
 {}
