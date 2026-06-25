34f1167a7ca5a6cd05a3cf3ed590aa71ebc966bd
zub <ars1for1work@gmail.com>
2026-06-25 11:35:41 +0300

    refactor(functions.cpp): update argument parsing to handle 'in:' and 'out:' prefixes and ensure uniqueness

--- zubarev.arsenii/U1/functions.cpp
+++ zubarev.arsenii/U1/functions.cpp
@@ -1,4 +1,27 @@
 #include "functions.hpp"
 
-bool zubarev::detail::parse_args(int argc, char** argv, std::string& inName, std::string& outName)
-{}
+bool zubarev::detail::parse_args(int argc, char** argv, std::string& in_name, std::string& out_name)
+{
+  bool has_in = false;
+  bool has_out = false;
+
+  for (size_t i = 0; i < argc; ++i) {
+    std::string arg(argv[i]);
+    if (arg.find("in: ")) {
+      if (has_in) {
+        return false;
+      }
+      in_name = arg.substr(3);
+      has_in = true;
+    } else if (arg.find("out: ")) {
+      if (has_out) {
+        return false;
+      }
+      out_name = arg.substr(3);
+      has_out = true;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
