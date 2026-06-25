94a89c91f4722070ba40a7a6552cef883098ce62
jar <Jarafan3@gmail.com>
2026-06-25 11:06:57 +0300

    Add read input

--- afanasev.yaroslav/U1/main.cpp
+++ afanasev.yaroslav/U1/main.cpp
@@ -8,5 +8,23 @@ struct Person
 
 int main()
 {
+  Person * persons = nullptr;
+  size_t count = 0;
+
+
+  persons = nullptr;
+  size_t capacity = 0;
+  std::string line;
+  while (std::getline(in, line))
+  {
+    Person person;
+    if (parseLine(line, person))
+    {
+            addPerson(persons, count, capacity, person);
+    }
+  }
+
+
+
   return 0;
 }
