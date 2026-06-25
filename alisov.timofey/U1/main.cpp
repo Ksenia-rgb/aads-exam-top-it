b40581d43977212d0404b8a0e2392c31fce6c2ab
m0trex <timofeyalisov@gmail.com>
2026-06-25 13:08:28 +0300

    fix too many arg

--- alisov.timofey/U1/main.cpp
+++ alisov.timofey/U1/main.cpp
@@ -112,8 +112,9 @@ namespace alisov
 int main(int argc, char *argv[])
 {
   if (argc > 3) {
-    return 1;
+    return 0;
   }
+
   std::string in_file = "";
   std::string out_file = "";
   bool has_in = false;
