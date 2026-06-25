9d8578909f835621c59b5cf0b9af9c77ec775199
arseniymusorin@gmail.com <quaka@quaka.localdomain>
2026-06-25 11:53:24 +0300

    stdin reading

--- musorin.arseniy/U1/main.cpp
+++ musorin.arseniy/U1/main.cpp
@@ -1,1 +1,25 @@
-int main() {}
+#include <iostream>
+#include <string>
+#include "list.hpp"
+#include "person.hpp"
+int main()
+{
+  musorin::List< musorin::Person > records;
+  musorin::initList(records);
+  std::string line;
+  while (std::getline(std::cin, line))
+  {
+    musorin::Person person{0, ""};
+    if (musorin::parseLine(line, person))
+    {
+      musorin::pushBack(records, person);
+    }
+  }
+  for (const musorin::detail::Node< musorin::Person > * node = records.head;
+    node != nullptr; node = node->next)
+  {
+    std::cout << node->value.id << ' ' << node->value.info << '\n';
+  }
+  musorin::clear(records);
+  return 0;
+}
