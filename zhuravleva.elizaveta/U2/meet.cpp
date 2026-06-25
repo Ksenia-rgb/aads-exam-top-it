f9241f40be6deb9dcafa954c03d168e29f44ba40
elizazha <lizokbella@mail.ru>
2026-06-25 12:58:50 +0300

    Implement meetings input

--- zhuravleva.elizaveta/U2/meet.cpp
+++ zhuravleva.elizaveta/U2/meet.cpp
@@ -1,19 +1,146 @@
 #include "meet.hpp"
+#include <istream>
+#include <stdexcept>
 
-bool zhuravleva::parseArgs(int, char *[], Args &)
+namespace
 {
-  return false;
+  bool startsWith(const std::string &str, const char *prefix)
+  {
+    size_t i = 0;
+    while (prefix[i] != '\0')
+    {
+      if (i >= str.size() || str[i] != prefix[i])
+      {
+        return false;
+      }
+      ++i;
+    }
+    return true;
+  }
+
+  bool isSpace(char c)
+  {
+    return c == ' ' || c == '\t' || c == '\r'
+        || c == '\f' || c == '\v';
+  }
+
+  bool readNumber(const std::string &line, size_t &pos, size_t &value)
+  {
+    while (pos < line.size() && isSpace(line[pos]))
+    {
+      ++pos;
+    }
+    if (pos == line.size() || line[pos] < '0' || line[pos] > '9')
+    {
+      return false;
+    }
+    value = 0;
+    while (pos < line.size() && line[pos] >= '0' && line[pos] <= '9')
+    {
+      value = value * 10 + static_cast< size_t >(line[pos] - '0');
+      ++pos;
+    }
+    return true;
+  }
+
+  bool isEmptyLine(const std::string &line)
+  {
+    for (size_t i = 0; i < line.size(); ++i)
+    {
+      if (!isSpace(line[i]))
+      {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
-bool zhuravleva::readMeet(const std::string &, Meet &)
+bool zhuravleva::parseArgs(int argc, char *argv[], Args &args)
 {
-  return false;
+  args.personsFile = "";
+  args.meetsFile = "";
+  args.hasPersons = false;
+
+  if (argc < 2 || argc > 3)
+  {
+    return false;
+  }
+  bool hasData = false;
+  for (int i = 1; i < argc; ++i)
+  {
+    const std::string arg(argv[i]);
+
+    if (startsWith(arg, "in:"))
+    {
+      if (args.hasPersons || arg.size() == 3)
+      {
+        return false;
+      }
+      args.personsFile = arg.substr(3);
+      args.hasPersons = true;
+    }
+    else if (startsWith(arg, "data:"))
+    {
+      if (hasData || arg.size() == 5)
+      {
+        return false;
+      }
+      args.meetsFile = arg.substr(5);
+      hasData = true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  return hasData;
 }
 
-void zhuravleva::readMeets(std::istream &, DynamicArray< Meet > &)
-{}
 
-void zhuravleva::addAnonymous(
-    DynamicArray< Person > &,
-    const DynamicArray< Meet > &)
-{}
+bool zhuravleva::readMeet(const std::string &line, Meet &meet)
+{
+  size_t pos = 0;
+  if (!readNumber(line, pos, meet.first))
+  {
+    return false;
+  }
+
+  if (!readNumber(line, pos, meet.second))
+  {
+    return false;
+  }
+  if (!readNumber(line, pos, meet.time))
+  {
+    return false;
+  }
+  while (pos < line.size() && isSpace(line[pos]))
+  {
+    ++pos;
+  }
+  if (pos != line.size())
+  {
+    return false;
+  }
+  return meet.first != meet.second;
+}
+
+void zhuravleva::readMeets(
+    std::istream &input,
+    DynamicArray< Meet > &meets)
+{
+  std::string line;
+  while (std::getline(input, line))
+  {
+    if (line.empty())
+    {
+      continue;
+    }
+    Meet meet = {0, 0, 0};
+    if (isEmptyLine(line))
+    {
+      continue;
+    }
+    pushBack(meets, meet);
+  }
+}
