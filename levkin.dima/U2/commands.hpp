bcfa659bd5237ff94b01855fbaf92fab4ad2edf9
Dmitry Levkin <mit.levkin@vk.com>
2026-06-25 12:56:38 +0300

    Update commands.hpp

--- levkin.dima/U2/commands.hpp
+++ levkin.dima/U2/commands.hpp
@@ -3,16 +3,16 @@
 
 #include <iosfwd>
 
-class DB;
 namespace levkin {
-  void anons(DB db, std::istream& is, std::ostream& os);
-  void deanon(DB db, std::istream& is, std::ostream& os);
-  void redesc(DB db, std::istream& is, std::ostream& os);
-  void desc(DB db, std::istream& is, std::ostream& os);
-  void meets(DB db, std::istream& is, std::ostream& os);
-  void commons(DB db, std::istream& is, std::ostream& os);
-  void less(DB db, std::istream& is, std::ostream& os);
-  void greater(DB db, std::istream& is, std::ostream& os);
-  void outPersons(DB db, std::istream& is, std::ostream& os);
+  class DB;
+  void anons(const DB& db, std::ostream& os);
+  void deanon(DB& db, std::istream& is, std::ostream& os);
+  void redesc(DB& db, std::istream& is, std::ostream& os);
+  void desc(const DB& db, std::istream& is, std::ostream& os);
+  void meets(const DB& db, std::istream& is, std::ostream& os);
+  void commons(const DB& db, std::istream& is, std::ostream& os);
+  void less(const DB& db, std::istream& is, std::ostream& os);
+  void greater(const DB& db, std::istream& is, std::ostream& os);
+  void outPersons(const DB& db, std::istream& is, std::ostream& os);
 }
 #endif
