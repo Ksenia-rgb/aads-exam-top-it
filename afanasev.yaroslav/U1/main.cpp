ec0085d002d707747df98fa64098e91be349c9b2
jar <Jarafan3@gmail.com>
2026-06-25 11:49:18 +0300

    Add namespace Done

--- afanasev.yaroslav/U1/main.cpp
+++ afanasev.yaroslav/U1/main.cpp
@@ -3,61 +3,68 @@
 #include <string>
 #include <sstream>
 
-struct Person
+namespace afanasev
 {
-  size_t id;
-  std::string info;
-};
-
-bool parseLine(const std::string & line, Person & person)
-{
-  std::istringstream iss(line);
-  if (!(iss >> person.id))
+  struct Person
   {
-    return false;
+    size_t id;
+    std::string info;
+  };
+
+  bool parseLine(const std::string & line, Person & person)
+  {
+    std::istringstream iss(line);
+    if (!(iss >> person.id))
+    {
+      return false;
+    }
+
+    iss >> std::ws;
+    if (!std::getline(iss, person.info))
+    {
+      return false;
+    }
+    return !person.info.empty();
   }
 
-  iss >> std::ws;
-  if (!std::getline(iss, person.info))
+  void addPerson(Person *& persons, size_t & count, size_t & capacity, const Person & person)
   {
-    return false;
+    if (count == capacity)
+    {
+      size_t newCapacity = capacity == 0 ? 1 : capacity * 2;
+      Person * newPersons = new Person[newCapacity];
+
+      for (size_t i = 0; i < count; ++i)
+      {
+        newPersons[i] = persons[i];
+      }
+
+      delete[] persons;
+      persons = newPersons;
+      capacity = newCapacity;
+    }
+    persons[count++] = person;
   }
-  return !person.info.empty();
-}
 
-void addPerson(Person *& persons, size_t & count, size_t & capacity, const Person & person)
-{
-  if (count == capacity)
+  bool existsPerson(const Person * persons, size_t count, size_t id)
   {
-    size_t newCapacity = capacity == 0 ? 1 : capacity * 2;
-    Person * newPersons = new Person[newCapacity];
-
     for (size_t i = 0; i < count; ++i)
     {
-      newPersons[i] = persons[i];
+      if (persons[i].id == id)
+      {
+        return true;
+      }
     }
-
-    delete[] persons;
-    persons = newPersons;
-    capacity = newCapacity;
+    return false;
   }
-  persons[count++] = person;
 }
 
-bool existsPerson(const Person * persons, size_t count, size_t id)
-{
-  for (size_t i = 0; i < count; ++i)
-  {
-    if (persons[i].id == id)
-    {
-      return true;
-    }
-  }
-  return false;
-}
+
 
 int main(int argc, char * argv[])
 {
+  using namespace afanasev;
+
   if (argc > 3)
   {
     std::cerr << "Too many arguments\n";
