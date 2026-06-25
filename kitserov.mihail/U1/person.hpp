0dd33f0b913b045935dc622fe431afee02f19313
Kreniker <kitserov.mike@gmail.com>
2026-06-25 11:01:35 +0300

    add person and contains

--- kitserov.mihail/U1/person.hpp
+++ kitserov.mihail/U1/person.hpp
@@ -16,13 +16,40 @@ namespace kitserov
     size_t size;
     size_t capacity;
   };
-  bool contains(const PersonsContainer& container, size_t id) {
+  bool contains(const PersonsContainer& container, size_t id)
+  {
     for (size_t i = 0; i < container.size; ++i) {
-        if (container.data[i].id == id) {
-            return true;
-        }
+      if (container.data[i].id == id) {
+        return true;
+      }
     }
     return false;
+  }
+  bool addPerson(PersonsContainer& container, size_t id, const std::string& info)
+  {
+    if (contains(container, id)) {
+      return false;
+    }
+    if (container.size == container.capacity) {
+      size_t new_cap = (container.capacity == 0) ? 8 : container.capacity * 2;
+      Person* new_data = new Person[new_cap];
+      try {
+        for (size_t i = 0; i < container.size; ++i) {
+          new_data[i] = container.data[i];
+        }
+        new_data[container.size] = Person{id, info};
+      } catch (...) {
+        delete[] new_data;
+        throw; 
+      }
+      delete[] container.data;
+      container.data = new_data;
+      container.capacity = new_cap;
+    } else {
+      container.data[container.size] = Person{id, info};
+    }
+    container.size++;
+    return true;
   }
 }
 
