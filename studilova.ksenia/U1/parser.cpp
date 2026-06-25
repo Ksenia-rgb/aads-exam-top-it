57ae36e227f5f0f05fa7b669cbb5945f731a2f6a
kstudilova <kstudilova@gmail.com>
2026-06-25 12:18:15 +0300

    check spaces functions

--- studilova.ksenia/U1/parser.cpp
+++ studilova.ksenia/U1/parser.cpp
@@ -13,6 +13,27 @@ namespace
   {
     return std::isdigit(static_cast< unsigned char >(symbol));
   }
+
+  void skipSpaces(const std::string &line, size_t &position)
+  {
+    while ((position < line.size()) && isSpace(line[position]))
+    {
+      ++position;
+    }
+  }
+
+   bool hasNoSpace(const std::string &line, size_t position)
+  {
+    while (position < line.size())
+    {
+      if (!isSpace(line[position]))
+      {
+        return true;
+      }
+      ++position;
+    }
+    return false;
+  }
 }
 
 bool studilova::parsePersonLine(const std::string &line, Person &person)
