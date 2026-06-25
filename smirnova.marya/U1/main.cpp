ddd89287a44998508a1278d34af9cd1aa58a033a
SmirnovaMarya <unlenmaria@gmail.com>
2026-06-25 13:45:50 +0300

    all

--- smirnova.marya/U1/main.cpp
+++ smirnova.marya/U1/main.cpp
@@ -82,7 +82,7 @@ int main(int argc, char* argv[])
     smirnova::printPersons(persons, std::cout);
   }
 
-  std::cerr << validEntries << " " << ignoredEntries << std::endl;
+  std::cerr << validEntries << " " << ignoredEntries << "\n";
 
   return 0;
 }
