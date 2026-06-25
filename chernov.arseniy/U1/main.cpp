cb45eccf4ec6156f1cc33a0858f7058eeeb804b3
aaaa24 <chernov.arseniy@yandex.ru>
2026-06-25 11:25:45 +0300

    add input args

--- chernov.arseniy/U1/main.cpp
+++ chernov.arseniy/U1/main.cpp
@@ -1,4 +1,33 @@
 #include <iostream>
+#include <vector.hpp>
+#include "input.hpp"
+#include "person.hpp"
 
-int main()
-{}
+int main(int argc, char ** argv)
+{
+  using namespace chernov;
+
+  std::string inFile, outFile;
+  if (!parseArgs(argc, argv, inFile, outFile)) {
+    std::cerr << "Invalid arguments\n";
+    return 1;
+  }
+
+  std::ifstream fin;
+  if (!inFile.empty()) {
+    fin.open(inFile);
+    if (!fin.is_open()) {
+      std::cerr << "Cannot open input file\n";
+      return 2;
+    }
+  }
+
+  std::ofstream fout;
+  if (!outFile.empty()) {
+    fout.open(outFile);
+    if (!fout.is_open()) {
+      std::cerr << "Cannot open output file\n";
+      return 2;
+    }
+  }
+}
