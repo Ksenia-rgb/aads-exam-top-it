7e9993a455d49a6e5a6b551a3edc19352d12091e
CWSHBR <mrcookie209@gmail.com>
2026-06-25 11:37:06 +0300

    add person io
    Read and write Person records while preserving input order and counting ignored lines.

--- shaykhraziev.bulat/U1/test-person-io.cpp
+++ shaykhraziev.bulat/U1/test-person-io.cpp
@@ -1,6 +1,46 @@
 #include <boost/test/unit_test.hpp>
+#include <cstdio>
+#include <fstream>
 #include "../common/domain.hpp"
+#include "../common/hash-table.hpp"
+#include "../common/list.hpp"
 #include "../common/parse.hpp"
+#include "../common/person-io.hpp"
+
+namespace
+{
+  void writeTextFile(const char* filename, const char* text)
+  {
+    std::ofstream output(filename);
+    output << text;
+  }
+
+  std::string readTextFile(const char* filename)
+  {
+    std::ifstream input(filename);
+    std::string result;
+    char symbol = '\0';
+    while (input.get(symbol))
+    {
+      result += symbol;
+    }
+    return result;
+  }
+
+  void initPersonStorage(shaykhraziev::List< shaykhraziev::Person >& persons,
+      shaykhraziev::HashTable< size_t, shaykhraziev::Person* >& personsById)
+  {
+    shaykhraziev::initList(persons);
+    shaykhraziev::initHashTable(personsById, 17, shaykhraziev::hashSizeT, shaykhraziev::equalSizeT);
+  }
+
+  void clearPersonStorage(shaykhraziev::List< shaykhraziev::Person >& persons,
+      shaykhraziev::HashTable< size_t, shaykhraziev::Person* >& personsById)
+  {
+    shaykhraziev::clearHashTable(personsById);
+    shaykhraziev::clearList(persons);
+  }
+}
 
 BOOST_AUTO_TEST_CASE(parse_correct_person_line)
 {
@@ -142,4 +182,110 @@ BOOST_AUTO_TEST_CASE(person_has_required_fields)
 
   BOOST_TEST(person.id == 1);
   BOOST_TEST(person.info == "info");
+}
+
+BOOST_AUTO_TEST_CASE(add_and_find_person)
+{
+  shaykhraziev::List< shaykhraziev::Person > persons;
+  shaykhraziev::HashTable< size_t, shaykhraziev::Person* > personsById;
+  initPersonStorage(persons, personsById);
+  const shaykhraziev::Person person = { 10, "Known" };
+
+  BOOST_TEST(shaykhraziev::addPerson(persons, personsById, person));
+  shaykhraziev::Person* found = shaykhraziev::findPersonById(personsById, 10);
+
+  BOOST_TEST(found != nullptr);
+  BOOST_TEST(found->id == 10);
+  BOOST_TEST(found->info == "Known");
+
+  clearPersonStorage(persons, personsById);
+}
+
+BOOST_AUTO_TEST_CASE(reject_duplicate_person)
+{
+  shaykhraziev::List< shaykhraziev::Person > persons;
+  shaykhraziev::HashTable< size_t, shaykhraziev::Person* > personsById;
+  initPersonStorage(persons, personsById);
+  const shaykhraziev::Person first = { 10, "Known" };
+  const shaykhraziev::Person second = { 10, "Other" };
+
+  BOOST_TEST(shaykhraziev::addPerson(persons, personsById, first));
+  BOOST_TEST(!shaykhraziev::addPerson(persons, personsById, second));
+  BOOST_TEST(persons.size == 1);
+
+  clearPersonStorage(persons, personsById);
+}
+
+BOOST_AUTO_TEST_CASE(read_persons_full_example)
+{
+  const char* inputName = "out/u1-read-persons-input.txt";
+  writeTextFile(inputName,
+      "100 Sneaky person\n"
+      "23 Deadman\n"
+      "31 The Agent\n"
+      "56 \"Scarface\"\n"
+      "99\n"
+      "31 Mr. Bond\n");
+  std::ifstream input(inputName);
+  shaykhraziev::List< shaykhraziev::Person > persons;
+  shaykhraziev::HashTable< size_t, shaykhraziev::Person* > personsById;
+  initPersonStorage(persons, personsById);
+  size_t accepted = 0;
+  size_t ignored = 0;
+
+  shaykhraziev::readPersons(input, persons, personsById, accepted, ignored);
+
+  BOOST_TEST(accepted == 4);
+  BOOST_TEST(ignored == 2);
+  BOOST_TEST(persons.size == 4);
+  BOOST_TEST(shaykhraziev::findPersonById(personsById, 31)->info == "The Agent");
+
+  clearPersonStorage(persons, personsById);
+  std::remove(inputName);
+}
+
+BOOST_AUTO_TEST_CASE(write_persons_preserves_order)
+{
+  const char* outputName = "out/u1-write-persons-output.txt";
+  shaykhraziev::List< shaykhraziev::Person > persons;
+  shaykhraziev::HashTable< size_t, shaykhraziev::Person* > personsById;
+  initPersonStorage(persons, personsById);
+  shaykhraziev::addPerson(persons, personsById, shaykhraziev::Person{ 100, "Sneaky person" });
+  shaykhraziev::addPerson(persons, personsById, shaykhraziev::Person{ 23, "Deadman" });
+  shaykhraziev::addPerson(persons, personsById, shaykhraziev::Person{ 31, "The Agent" });
+  std::ofstream output(outputName);
+
+  shaykhraziev::writePersons(output, persons);
+  output.close();
+
+  BOOST_TEST(readTextFile(outputName) == "100 Sneaky person\n23 Deadman\n31 The Agent\n");
+
+  clearPersonStorage(persons, personsById);
+  std::remove(outputName);
+}
+
+BOOST_AUTO_TEST_CASE(read_persons_ignores_bad_lines)
+{
+  const char* inputName = "out/u1-read-bad-persons-input.txt";
+  writeTextFile(inputName,
+      "abc Broken\n"
+      "100\n"
+      "100 Known\n"
+      "100 Duplicate\n");
+  std::ifstream input(inputName);
+  shaykhraziev::List< shaykhraziev::Person > persons;
+  shaykhraziev::HashTable< size_t, shaykhraziev::Person* > personsById;
+  initPersonStorage(persons, personsById);
+  size_t accepted = 0;
+  size_t ignored = 0;
+
+  shaykhraziev::readPersons(input, persons, personsById, accepted, ignored);
+
+  BOOST_TEST(accepted == 1);
+  BOOST_TEST(ignored == 3);
+  BOOST_TEST(persons.size == 1);
+  BOOST_TEST(shaykhraziev::findPersonById(personsById, 100)->info == "Known");
+
+  clearPersonStorage(persons, personsById);
+  std::remove(inputName);
 }
---
+++ shaykhraziev.bulat/common/person-io.cpp
@@ -0,0 +1,84 @@
+#include "../common/person-io.hpp"
+#include <istream>
+#include <ostream>
+#include "../common/parse.hpp"
+
+shaykhraziev::Person* shaykhraziev::findPersonById(HashTable< size_t, Person* >& personsById,
+    size_t id)
+{
+  Person** person = find(personsById, id);
+  if (person == nullptr)
+  {
+    return nullptr;
+  }
+  return *person;
+}
+
+bool shaykhraziev::addPerson(List< Person >& persons,
+    HashTable< size_t, Person* >& personsById,
+    const Person& person)
+{
+  if (findPersonById(personsById, person.id) != nullptr)
+  {
+    return false;
+  }
+  Person* inserted = pushBack(persons, person);
+  try
+  {
+    if (!insert(personsById, person.id, inserted))
+    {
+      popBack(persons);
+      return false;
+    }
+  }
+  catch (...)
+  {
+    popBack(persons);
+    throw;
+  }
+  return true;
+}
+
+void shaykhraziev::readPersons(std::istream& input,
+    List< Person >& persons,
+    HashTable< size_t, Person* >& personsById,
+    size_t& accepted,
+    size_t& ignored)
+{
+  accepted = 0;
+  ignored = 0;
+  std::string line;
+  while (std::getline(input, line))
+  {
+    size_t id = 0;
+    size_t position = 0;
+    std::string description;
+    if (!parseSizeTPrefix(line, id, position) || !readRestDescription(line, position, description))
+    {
+      ++ignored;
+    }
+    else
+    {
+      const Person person = { id, description };
+      if (addPerson(persons, personsById, person))
+      {
+        ++accepted;
+      }
+      else
+      {
+        ++ignored;
+      }
+    }
+  }
+}
+
+void shaykhraziev::writePersons(std::ostream& output, List< Person >& persons)
+{
+  ListIterator< Person > iterator = begin(persons);
+  while (!isEnd(iterator))
+  {
+    const Person& person = getConst(iterator);
+    output << person.id << ' ' << person.info << '\n';
+    iterator = next(iterator);
+  }
+}
