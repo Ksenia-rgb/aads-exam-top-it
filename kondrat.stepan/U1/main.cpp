6ddb824d760c518814dc18ba62e09e264c8b7853
StKondrat <kndrt13@icloud.com>
2026-06-25 11:09:53 +0300

    add Person and other help structs

--- kondrat.stepan/U1/main.cpp
+++ kondrat.stepan/U1/main.cpp
@@ -1,2 +1,27 @@
+#include <cstddef>
+#include <string>
+
+namespace kondrat
+{
+  struct Person
+  {
+    size_t id;
+    std::string info;
+  };
+
+  struct PersonStorage
+  {
+    Person * data;
+    size_t size;
+    size_t capacity;
+  };
+
+  struct ProgramArgs
+  {
+    const char * inputName;
+    const char * outputName;
+  };
+}
+
 int main()
 {}
