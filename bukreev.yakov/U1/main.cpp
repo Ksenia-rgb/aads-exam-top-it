bf8ffbd6d7401abf7a0eb330bf041a95a2ac354d
BukreevYas <bukreev.yas@edu.spbstu.ru>
2026-06-25 11:32:41 -0400

    Refine main

--- bukreev.yakov/U1/main.cpp
+++ bukreev.yakov/U1/main.cpp
@@ -1,16 +1,19 @@
 #include "list.hpp"
 #include <string>
 
-struct Person
+namespace bukreev
 {
-  size_t id;
-  std::string info;
-};
-
-bukreev::List< Person >* people;
+  struct Person
+  {
+    size_t id;
+    std::string info;
+  };
+}
 
 int main()
 {
+  bukreev::List< bukreev::Person >* people;
+
   bukreev::createList(&people);
   bukreev::deleteList(people);
 }
