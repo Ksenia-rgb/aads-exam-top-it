4d127f0f489a5b843d27341a0c3d54679ed7335c
EGGorkaa <egor_novikov20004@mail.ru>
2026-06-25 13:19:35 +0300

    CG

--- novikov.egor/U1/List.hpp
+++ novikov.egor/U1/List.hpp
@@ -3,21 +3,18 @@
 
 namespace novikov
 {
-  template < class T >
-  struct Node
+  template < class T > struct Node
   {
     T val;
     Node< T > *next;
   };
 
-  template < class T >
-  struct List
+  template < class T > struct List
   {
     Node< T > *head;
   };
 
-  template < class T >
-  void push_back(List< T > &list, const T &val)
+  template < class T > void push_back(List< T > &list, const T &val)
   {
     Node< T > *node = new Node< T >{val, nullptr};
     if (!list.head) {
@@ -31,13 +28,11 @@ namespace novikov
     }
   };
 
-  template < class T >
-  void clear(List<T>& list)
+  template < class T > void clear(List< T > &list)
   {
-    Node<T>* cur = list.head;
-    while(cur)
-    {
-      Node<T>* next = cur->next;
+    Node< T > *cur = list.head;
+    while (cur) {
+      Node< T > *next = cur->next;
       delete cur;
       cur = next;
     }
