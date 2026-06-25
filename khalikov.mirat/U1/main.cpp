fb1d6bb4c1110c89fef4419e6a8dd87a2c61ed84
khalii21 <miratkhalikov007@gmail.com>
2026-06-25 11:35:49 +0300

    List added

--- khalikov.mirat/U1/main.cpp
+++ khalikov.mirat/U1/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <string>
+
+namespace khalikov {
+  void InRead(std::)
 
 int main(int argc, char **argv)
 {
   if (argc > 3) {
     return 1;
   }
+
+  std::ifstream input = argv[];
+  std::ofstream output;
+  std::string line
+  while (std::getline(std::cin, line))
 }
---
+++ khalikov.mirat/U1/person.hpp
@@ -0,0 +1,11 @@
+#ifndef PERSON_HPP
+#define PERSON_HPP
+
+#include <string>
+
+namespace khalikov {
+  struct Person
+  {
+    size_t id;
+    std::string info;
+  }
