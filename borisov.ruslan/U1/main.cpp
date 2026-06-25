61d360c9151091235858e1d4db4d187f5bc5d343
RBorisov6 <borisovruslan1512@gmail.com>
2026-06-25 12:48:03 +0300

    fix to many args and empty input

--- borisov.ruslan/U1/main.cpp
+++ borisov.ruslan/U1/main.cpp
@@ -5,6 +5,10 @@
 
 int main(int argc, char **argv)
 {
+  if (argc > 3)
+  {
+    return 0;
+  }
   const borisov::Args args = borisov::parseArgs(argc, argv);
   if (!args.valid_)
   {
@@ -22,8 +26,12 @@ int main(int argc, char **argv)
   std::istream &in = args.inSet_ ? static_cast< std::istream & >(inFstream) : std::cin;
   size_t count = 0;
   size_t skipped = 0;
-  borisov::PersonNode * const head = borisov::readPersons(in, count, skipped);
-  inFstream.close();
+  bool hasInput = false;
+  borisov::PersonNode * const head = borisov::readPersons(in, count, skipped, hasInput);
+  if (args.inSet_)
+  {
+    inFstream.close();
+  }
   std::ofstream outFstream;
   if (args.outSet_)
   {
@@ -37,7 +45,7 @@ int main(int argc, char **argv)
   std::ostream &out = args.outSet_ ? static_cast< std::ostream & >(outFstream) : std::cout;
   borisov::writePersons(out, head);
   borisov::freePersons(head);
-  if (count > 0 || skipped > 0)
+  if (argc > 1 || hasInput)
   {
     std::cerr << count << " " << skipped << "\n";
   }
