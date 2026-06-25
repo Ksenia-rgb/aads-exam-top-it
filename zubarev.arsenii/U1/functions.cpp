62f1ab2056360ddc65aa70983ccbcfff32d02d39
zub <ars1for1work@gmail.com>
2026-06-25 12:21:37 +0300

    fix(functions.cpp): adjust argument parsing to start from index 1 and check prefixes strictly

--- zubarev.arsenii/U1/functions.cpp
+++ zubarev.arsenii/U1/functions.cpp
@@ -9,15 +9,15 @@ bool zubarev::detail::parse_args(int argc, char** argv, std::string& in_name, st
   bool has_in = false;
   bool has_out = false;
 
-  for (int i = 0; i < argc; ++i) {
+  for (int i = 1; i < argc; ++i) {
     std::string arg(argv[i]);
-    if (arg.find("in:")) {
+    if (arg.find("in:") == 0) {
       if (has_in) {
         return false;
       }
       in_name = arg.substr(3);
       has_in = true;
-    } else if (arg.find("out:")) {
+    } else if (arg.find("out:") == 0) {
       if (has_out) {
         return false;
       }
