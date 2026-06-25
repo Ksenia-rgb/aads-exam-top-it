0bf5ab7069a614949124f887618b7641a8472ca3
Владислав  Шигарев <vladislavsigarev@MBPWiFi.local>
2026-06-25 11:26:49 +0300

    Add destroyDynArray and grow operations

--- shigarev.vladislav/U1/dynarray.hpp
+++ shigarev.vladislav/U1/dynarray.hpp
@@ -1,4 +1,4 @@
-ifndef DYNARRAY_HPP
+#ifndef DYNARRAY_HPP
 #define DYNARRAY_HPP
  
 #include <cstddef>
@@ -21,5 +21,34 @@ namespace shigarev {
       result.capacity_ = capacity;
     }
     return result;
+  }
+ 
+  template< class T >
+  void destroyDynArray(DynArray< T >& arr)
+  {
+    delete[] arr.data_;
+    arr.data_ = nullptr;
+    arr.size_ = 0;
+    arr.capacity_ = 0;
+  }
+ 
+  namespace detail {
+    template< class T >
+    void grow(DynArray< T >& arr)
+    {
+      const size_t newCap = (arr.capacity_ == 0) ? 4 : arr.capacity_ * 2;
+      T* newData = new T[newCap];
+      try {
+        for (size_t i = 0; i < arr.size_; ++i) {
+          newData[i] = std::move(arr.data_[i]);
+        }
+      } catch (...) {
+        delete[] newData;
+        throw;
+      }
+      delete[] arr.data_;
+      arr.data_ = newData;
+      arr.capacity_ = newCap;
+    }
   }
 }
